40a26ec7e4cb7e92ed639651294feff1b6f414e9
Doug Mills <slipdigital@gmail.com>
2019-02-04 23:44:19 +0000

    add account controller

---
+++ Medical_Examiner_API/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Medical_Examiner_API.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+
+    }
+}
