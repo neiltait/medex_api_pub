c614f1aa82ff2f5d9da63171d43f64d9b1b4175c
DavidJohnParsons <47145770+DavidJohnParsons@users.noreply.github.com>
2019-02-12 16:45:07 +0000

    Added mocker class and unit test

---
+++ Medical_Examiner_API/Loggers/MELoggerMocker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Medical_Examiner_API.Loggers
+{
+    public class MELoggerMocker : IMELogger
+    {
+        public string Message { get; private set; }
+        public void Log(string message)
+        {
+            Message = "MELoggerMocker.Log() called";
+        }
+    }
+}
---
+++ Medical_Examiner_API_Tests/ControllerTests/DJPTestControllerTests.cs
@@ -0,0 +1,37 @@
+﻿
+using FluentAssertions;
+using ME_API_tests.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Medical_Examiners_API.Controllers;
+using Medical_Examiner_API.Controllers;
+using Medical_Examiner_API.Loggers;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Medical_Examiner_API_Tests.ControllerTests
+{
+    public class DJPTestControllerTests
+    {
+        MELoggerMocker _mockLogger;
+        DJPTestController _controller;
+
+        public DJPTestControllerTests()
+        {
+            _mockLogger = new MELoggerMocker();
+            _controller = new DJPTestController(_mockLogger); 
+        }
+
+        [Fact]
+        public void CheckCallToLogger()
+        {
+            _controller.Get();
+
+            var message = _mockLogger.Message;
+
+            var djp = 1;
+        }
+    }
+}
