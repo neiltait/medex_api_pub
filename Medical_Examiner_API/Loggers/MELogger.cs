1f438d9889737e552de210324594be8e667a8bd8
DavidJohnParsons <47145770+DavidJohnParsons@users.noreply.github.com>
2019-02-13 16:07:08 +0000

    Added streamwriter to MELogger

--- Medical_Examiner_API/Loggers/MELogger.cs
+++ Medical_Examiner_API/Loggers/MELogger.cs
@@ -2,14 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Medical_Examiner_API.Loggers
 {
     public class MELogger : IMELogger
     {
+        private StreamWriter _streamWriter;
+        public MELogger()
+        {
+            var projectFolder = Environment.CurrentDirectory;
+            _streamWriter = new StreamWriter(projectFolder + "MElogging.txt");
+
+        }
+
         public void Log(string userName, string userAuthenticationType, string userIsAuthenticated, string controllerName, string controllerMethod, IList<string> parameters, string remoteIP, DateTime timeStamp)
         {
-            var djpToDo = 1;
+            var message = timeStamp.ToLongDateString() + " "+ timeStamp.ToShortTimeString() + " " + userName + " " + userAuthenticationType + " " + userIsAuthenticated + " " + controllerName + " " + controllerMethod + " ";
+
+            foreach (var p in parameters)
+            {
+                message += p + " ";
+            }
+
+            message += remoteIP;
+            _streamWriter.WriteLine(message);
+            _streamWriter.Flush();
         }
+
+
+       
     }
 }
