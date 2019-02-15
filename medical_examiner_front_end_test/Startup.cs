29b441b4b7eab0531b7489332229fb55dc9322c0
Doug Mills <slipdigital@gmail.com>
2019-02-15 10:46:23 +0000

    remove config for authenticatrion

--- medical_examiner_front_end_test/Startup.cs
+++ medical_examiner_front_end_test/Startup.cs
@@ -25,36 +25,16 @@ namespace medical_examiner_front_end_test
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(sharedOptions =>
-            {
-                sharedOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                sharedOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(options =>
-            {
-                options.Authority = "https://***REMOVED***.oktapreview.com/oauth2/default";
-                options.Audience = "api://default";
-            });
-            
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
+            app.UseDeveloperExceptionPage();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
