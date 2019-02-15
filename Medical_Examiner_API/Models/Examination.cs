037ad69d532ba03bda9e7a27949792c615568804
Douglas Mills <slipdigital@gmail.com>
2019-02-15 09:03:33 +0000

    added additional fields to examination

--- Medical_Examiner_API/Models/Examination.cs
+++ Medical_Examiner_API/Models/Examination.cs
@@ -7,9 +7,44 @@ namespace Medical_Examiner_API.Models
     public class Examination : Record
     {
         [JsonProperty(PropertyName = "id")]
-
         [Required]
         public string Id { get; set; }
+
+        [Required]
+        public string NHSNumber { get; set; }
+
+        [Required]
+        public int Gender { get; set; }
+
+        [Required]
+        public string HouseNameNumber { get; set; }
+
+        [Required]
+        public string Street { get; set; }
+
+        [Required]
+        public string Town { get; set; }
+
+        [Required]
+        public string County { get; set; }
+
+        [Required]
+        public string Postcode { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        public string LastOccupation { get; set; }
+
+        [Required]
+        public string OrganisationCareBeforeDeathLocationId { get; set; }
+
+        [Required]
+        public Representative[] Representatives { get; set; }
+
+        [Required]
+        public string DeathOccuredLocationId { get; set; }
 
         [Required]
         [StringLength(250)]
@@ -26,6 +61,19 @@ namespace Medical_Examiner_API.Models
         [Required]
         [DataType(DataType.DateTime)]
         public DateTimeOffset DateOfDeath { get; set; }
+
+        [Required]
+        public bool PriorityDeath { get; set; }
+
+        [Required]
+        public bool IsChild { get; set; }
+
+        [Required]
+        public bool ReferToCoroner { get; set; }
+
+        [Required]
+        public bool PriorityCase { get; set; }
+
 
         [Required]
         public bool Completed { get; set; }
---
+++ Medical_Examiner_API/Models/Representative.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace Medical_Examiner_API.Models
+{
+    public class Representative
+    {
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public string RelationshipToDeceased { get; set; }
+
+        [Required]
+        public string ContactPhoneNumber { get; set; }
+
+        [Required]
+        public bool PresentAtDeath { get; set; }
+
+        [Required]
+        public bool InformedOfDeath { get; set; }
+    }
+}
