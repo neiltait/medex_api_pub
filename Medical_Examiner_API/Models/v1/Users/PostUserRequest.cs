91c246e25bdd6aedb37919c30fe38dfe2ac6b7ea
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-02-20 11:19:26 +0000

    V1 Fix

--- Medical_Examiner_API/Models/v1/Users/PostUserRequest.cs
+++ Medical_Examiner_API/Models/v1/Users/PostUserRequest.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Medical_Examiner_API.Models.v1.Users
+namespace Medical_Examiner_API.Models.V1.Users
 {
     public class PostUserRequest
     {
