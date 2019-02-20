6f31f1f8f357dc17e6938fdcc151df8bd2e8875d
DavidJohnParsons <47145770+DavidJohnParsons@users.noreply.github.com>
2019-02-20 17:05:30 +0000

    WIP deal with warnings

--- Medical_Examiner_API/Seeders/LocationsSeeder.cs
+++ Medical_Examiner_API/Seeders/LocationsSeeder.cs
@@ -7,10 +7,20 @@ using Newtonsoft.Json;
 
 namespace Medical_Examiner_API.Seeders
 {
+    /// <summary>
+    /// Adds location objects to database from source file
+    /// </summary>
     public class LocationsSeeder
     {
+        /// <summary>
+        /// List of location objects that this class will create
+        /// </summary>
         public List<Location> Locations { get; private set; }
 
+        /// <summary>
+        /// Create Locations from file
+        /// </summary>
+        /// <param name="jsonFileName">full name of source path</param>
         public void LoadFromFile(string jsonFileName)
         {
             var json = File.ReadAllText(jsonFileName);
