f68ce39b1f9f5193c8199d4f6b21b93c8dae3f9b
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-02-20 09:51:59 +0000

    Response base extension for merging model errros into our generic error list

---
+++ Medical_Examiner_API/Extensions/Models/ResponseBaseExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Medical_Examiner_API.Models.v1;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Medical_Examiner_API.Extensions.Models
+{
+    /// <summary>
+    /// Response Base Extensions
+    /// </summary>
+    public static class ResponseBaseExtensions
+    {
+        /// <summary>
+        /// Add the model state errors to the response.
+        /// </summary>
+        /// <param name="responseBase">The response base.</param>
+        /// <param name="modelState">The model state.</param>
+        public static void AddModelErrors(this ResponseBase responseBase, ModelStateDictionary modelState)
+        {
+            foreach (var item in modelState)
+            {
+                foreach (var error in item.Value.Errors)
+                {
+                    responseBase.AddError(item.Key, error.ErrorMessage);
+                }
+            }
+        }
+    }
+}
