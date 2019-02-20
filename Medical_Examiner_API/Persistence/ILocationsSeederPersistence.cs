81bde5b169036116f6ef8c27b46e4b89f255bc84
DavidJohnParsons <47145770+DavidJohnParsons@users.noreply.github.com>
2019-02-20 15:21:18 +0000

    WIP sort out build warnings

--- Medical_Examiner_API/Persistence/ILocationsSeederPersistence.cs
+++ Medical_Examiner_API/Persistence/ILocationsSeederPersistence.cs
@@ -6,9 +6,16 @@ using Medical_Examiner_API.Seeders;
 
 namespace Medical_Examiner_API.Persistence
 {
-    interface ILocationsSeederPersistence
+    /// <summary>
+    /// Interface for persistence class used by location seeder
+    /// </summary>
+    public interface ILocationsSeederPersistence
     {
-
+        /// <summary>
+        /// Writes location object to database
+        /// </summary>
+        /// <param name="location">location object to be written to database</param>
+        /// <returns>bool</returns>
         Task<bool> SaveLocationAsync(Location location);
     }
 }
--- Medical_Examiner_API/Persistence/IMELoggerPersistence.cs
+++ Medical_Examiner_API/Persistence/IMELoggerPersistence.cs
@@ -7,8 +7,16 @@ using Medical_Examiner_API.Models;
 
 namespace Medical_Examiner_API.Persistence
 {
+    /// <summary>
+    /// Interface for persistence class used by MELogger
+    /// </summary>
     public interface IMELoggerPersistence
     {
+        /// <summary>
+        /// Writes log entry to database
+        /// </summary>
+        /// <param name="logEntry">object to be logged</param>
+        /// <returns>bool</returns>
         Task<bool> SaveLogEntryAsync(LogMessageActionDefault logEntry);
     }
 }
