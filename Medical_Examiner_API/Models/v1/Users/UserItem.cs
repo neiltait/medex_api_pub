a8e633805aec956b855c62eb27a39ee8353f4429
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-02-20 09:50:56 +0000

    Example user item for returning multiple users.

---
+++ Medical_Examiner_API/Models/v1/Users/UserItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Medical_Examiner_API.Models.v1.Users
+{
+    /// <summary>
+    /// A user item as part of multiple user responses.
+    /// </summary>
+    public class UserItem
+    {
+        /// <summary>
+        /// The User identifier.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The User's first name.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// The User's last name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The User's email address
+        /// </summary>
+        public string Email { get; set; }
+    }
+}
