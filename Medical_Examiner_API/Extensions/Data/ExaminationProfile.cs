9089d7929f78ab84ea8df9997d5614a177d3e2ae
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-02-21 12:31:06 +0000

    Add examination automapper profile and tests.

---
+++ Medical_Examiner_API/Extensions/Data/ExaminationProfile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Medical_Examiner_API.Models;
+using Medical_Examiner_API.Models.V1.Examinations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Medical_Examiner_API.Extensions.Data
+{
+    /// <summary>
+    /// Examination Mapping Extensions.
+    /// </summary>
+    public class ExaminationProfile : Profile
+    {
+        /// <summary>
+        /// Initialise a new instance of the Examination Profile.
+        /// </summary>
+        public ExaminationProfile()
+        {
+            CreateMap<Examination, GetExaminationResponse>();
+        }
+    }
+}
