3e796aea9c7ff0b640c2b0131acb147804c64b28
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-02-21 12:30:46 +0000

    Add initial examination models based on data model.

---
+++ Medical_Examiner_API/Models/v1/Examinations/GetExaminationsResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Medical_Examiner_API.Models.v1.Examinations;
+
+namespace Medical_Examiner_API.Models.V1.Examinations
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Response object for a list of examinations.
+    /// </summary>
+    public class GetExaminationsResponse : ResponseBase
+    {
+        /// <summary>
+        /// List of Examinations.
+        /// </summary>
+        public IEnumerable<ExaminationItem> Examinations { get; set; }
+    }
+}
