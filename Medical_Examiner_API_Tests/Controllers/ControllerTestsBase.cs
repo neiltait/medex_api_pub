d15c78bea99d04d3fb509e63efc9cafd8438002a
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-02-21 14:52:35 +0000

    Add controller test base to make using mappers in the controllers easier to test.

---
+++ Medical_Examiner_API_Tests/Controllers/ControllerTestsBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using Medical_Examiner_API.Controllers;
+using Medical_Examiner_API.Extensions.Data;
+
+namespace Medical_Examiner_API_Tests.Controllers
+{
+    /// <summary>
+    /// Base class for controller tests.
+    /// </summary>
+    /// <typeparam name="T">Type of controller.</typeparam>
+    public class ControllerTestsBase<T>
+        where T : BaseController
+    {
+        /// <summary>
+        /// Initialise a new instance of this base class.
+        /// </summary>
+        public ControllerTestsBase()
+        {
+            var mapperConfiguration = new MapperConfiguration(config =>
+            {
+                config.AddMedicalExaminerProfiles();
+            });
+
+            Mapper = mapperConfiguration.CreateMapper();
+        }
+
+        /// <summary>
+        /// Real AutoMapper.
+        /// </summary>
+        protected IMapper Mapper { get; }
+
+        /// <summary>
+        /// The Controller being tested.
+        /// </summary>
+        protected T Controller { get; set; }
+    }
+}
