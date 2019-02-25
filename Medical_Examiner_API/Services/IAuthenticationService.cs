f793aa1a222311e2a69ddd5a4421a51b0ee73bb0
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-02-25 14:14:31 +0000

    Super basic authentication service that requires no credentials.

---
+++ Medical_Examiner_API/Services/IAuthenticationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Medical_Examiner_API.Services
+{
+    /// <summary>
+    /// Example Authentication Service that just always gives out tokens
+    /// </summary>
+    public interface IAuthenticationService
+    {
+        /// <summary>
+        /// Example Authentication that requires no input and generates a token
+        /// </summary>
+        /// <returns>A token.</returns>
+        string Authenticate();
+    }
+}
