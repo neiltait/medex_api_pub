53c7c6c5f7e90af92cb17826eba4751c515d5e2b
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-02-25 14:14:18 +0000

    Updated settings and settings class.

---
+++ Medical_Examiner_API/Helpers/AuthenticationSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Medical_Examiner_API.Helpers
+{
+    /// <summary>
+    /// Authentication Settings
+    /// </summary>
+    public class AuthenticationSettings
+    {
+        /// <summary>
+        /// Secret string used to generate keys
+        /// </summary>
+        public string Secret { get; set; }
+    }
+}
