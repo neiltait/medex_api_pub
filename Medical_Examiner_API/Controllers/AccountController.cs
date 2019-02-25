32a9f9478ef46bf182a64ae078860e78921a5fd7
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-02-25 14:18:15 +0000

    Example authentication and use in account controller.

--- Medical_Examiner_API/Controllers/AccountController.cs
+++ Medical_Examiner_API/Controllers/AccountController.cs
@@ -7,17 +7,22 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Okta.AspNetCore;
 using Medical_Examiner_API.Loggers;
+using Medical_Examiner_API.Services;
 using Microsoft.AspNetCore.Authorization;
 
 namespace Medical_Examiner_API.Controllers
 {
+    [Authorize]
     [Route("api/auth")]
     [ApiController]
     public class AccountController : BaseController
     {
-        public AccountController(IMELogger logger)
+        private readonly IAuthenticationService _authenticationService;
+
+        public AccountController(IMELogger logger, IAuthenticationService authenticationService)
             : base(logger)
         {
+            _authenticationService = authenticationService;
         }
 
         [Route("protected")]
@@ -25,6 +30,34 @@ namespace Medical_Examiner_API.Controllers
         public string Protected()
         {
             return "Only if you have a valid token!";
+        }
+
+        /// <summary>
+        /// Authenticate
+        /// </summary>
+        /// <returns>Token.</returns>
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate()
+        {
+            var token = _authenticationService.Authenticate();
+
+            if (token == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(token);
+        }
+
+        /// <summary>
+        /// Test Method
+        /// </summary>
+        /// <returns>True.</returns>
+        [HttpGet("test")]
+        public bool TestAuthenticate()
+        {
+            return true;
         }
 
         [HttpGet("login")]
