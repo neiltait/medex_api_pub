430f0fa6d02b7f8038afd7f95bdfd2bd711d2130
DavidJohnParsons <47145770+DavidJohnParsons@users.noreply.github.com>
2019-03-04 12:21:09 +0000

    Create project to load location data

--- MedicalExaminer.API.Tests/Persistence/LocationPersistenceFake.cs
+++ MedicalExaminer.API.Tests/Persistence/LocationPersistenceFake.cs
@@ -28,47 +28,47 @@ namespace MedicalExaminer.API.Tests.Persistence
             national.LocationId = "1";
             national.Code = "National";
             national.Name = "Medical Examiners National";
-            national.Parent = null;
+            //national.Parent = null;
 
             region1.LocationId = "2";
             region1.Code = "R1";
             region1.Name = "Region 1";
-            region1.Parent = "National";
+            //region1.Parent = "National";
 
             region2.LocationId = "3";
             region2.Code = "R2";
             region2.Name = "Region 2";
-            region2.Parent = "National";
+            //region2.Parent = "National";
 
             trust1.LocationId = "4";
             trust1.Code = "R0A";
             trust1.Name = "Manchester University Foundation Trust";
-            trust1.Parent = "R1";
+            //trust1.Parent = "R1";
 
             trust2.LocationId = "5";
             trust2.Code = "RKK";
             trust2.Name = "Grimsby Foundation Trust";
-            trust2.Parent = "R2";
+            //trust2.Parent = "R2";
 
             site1.LocationId = "6";
             site1.Code = "R0A01";
             site1.Name = "Old Mill Hospital";
-            site1.Parent = "T1";
+            //site1.Parent = "T1";
 
             site2.LocationId = "7";
             site2.Code = "R0A02";
             site2.Name = "St Agnes Hospital";
-            site2.Parent = "T1";
+            //site2.Parent = "T1";
 
             site3.LocationId = "8";
             site3.Code = "RKK01";
             site3.Name = "Leeds Road Hospital";
-            site3.Parent = "T2";
+            //site3.Parent = "T2";
 
             site4.LocationId = "9";
             site4.Code = "RKK02";
             site4.Name = "North Hospital";
-            site4.Parent = "T2";
+            //site4.Parent = "T2";
 
             _locations.Add(national);
             _locations.Add(region1);
---
+++ MedicalExaminer.ReferenceDataLoader/Loaders/LocationsLoader.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using MedicalExaminer.Models;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+
+namespace MedicalExaminer.ReferenceDataLoader.Loaders
+{
+    /// <summary>
+    /// Create collection in CosmosDB and load with locations extracted from input file
+    /// </summary>
+    public class LocationsLoader
+    {
+        private static Uri _endpointUri;
+        private static string _primaryKey;
+        private static string _databaseId;
+        private static string _importFile;
+        private static string _containerID;
+        private static DocumentClient _client;
+        private static Uri _documentCollectionUri;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="args">0: endpoint; 1: primary key; 3: databaseId; 4: import file; 5: container id</param>
+        /// <remarks>Set up details of Cosmos DB and connection to write to</remarks>
+        public LocationsLoader(string[] args)
+        {
+            Console.WriteLine("Setting up parameters for LocationsLoader...");
+
+            _endpointUri = new Uri(args[0]);
+            _primaryKey = args[1];
+            _databaseId = args[2];
+            _importFile = args[3];
+            _containerID = args[4];
+
+            Console.WriteLine("Parameters read OK for LocationsLoader...");
+        }
+
+        public  async Task Load()
+        {
+            await CreateCollection();
+            await LoadLocations();
+        }
+
+        /// <summary>
+        /// Create collection in database
+        /// </summary>
+        /// <returns></returns>
+        private async Task CreateCollection()
+        {
+            _client = new DocumentClient(_endpointUri, _primaryKey);
+            var documentCollection = new DocumentCollection() { Id = _containerID };
+            var databaseResult = await _client.CreateDatabaseIfNotExistsAsync(new Database { Id = _databaseId });
+            var databaseUri = UriFactory.CreateDatabaseUri(_databaseId);
+            _documentCollectionUri = UriFactory.CreateDocumentCollectionUri(_databaseId, _containerID);
+            await _client.CreateDocumentCollectionIfNotExistsAsync(databaseUri, documentCollection);
+        }
+
+        private static async Task LoadLocations()
+        {
+            Console.WriteLine("Loading locations ...");
+
+            var startTime = DateTime.Now;
+            var json = File.ReadAllText(_importFile);
+            var locations = JsonConvert.DeserializeObject<List<Location>>(json);
+
+            foreach (var location in locations)
+            {
+                await _client.UpsertDocumentAsync(_documentCollectionUri, location);
+            }
+
+            var endTime = DateTime.Now;
+            var processingTime = endTime - startTime;
+
+            Console.WriteLine($"{locations.Count} locations loaded");
+            Console.WriteLine($"Processing time: {processingTime.Hours} Hours;  {processingTime.Minutes} Minutes; {processingTime.Seconds} Seconds");
+            Console.ReadKey();
+
+        }
+    }
+}
