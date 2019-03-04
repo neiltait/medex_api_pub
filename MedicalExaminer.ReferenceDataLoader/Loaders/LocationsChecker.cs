ce59efe4909350d58d058ff296cdb4f60cb97b2e
DavidJohnParsons <47145770+DavidJohnParsons@users.noreply.github.com>
2019-03-04 14:22:58 +0000

    Adding LocationsChecker

---
+++ MedicalExaminer.ReferenceDataLoader/Loaders/LocationsChecker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MedicalExaminer.Models;
+using MedicalExaminer.Models.Enums;
+
+namespace MedicalExaminer.ReferenceDataLoader.Loaders
+{
+    /// <summary>
+    /// Check that locations are in concsistent state
+    /// </summary>
+    public class LocationsChecker
+    {
+        private List<Location> _locations;
+
+        public LocationsChecker(List<Location> locations)
+        {
+            _locations = locations;
+        }
+
+        /// <summary>
+        /// Check that every location has a parentId links to another location, except the national loaction where parentD should be null 
+        /// </summary>
+        /// <returns>bool</returns>
+        public bool CheckParentIdsValid()
+        {
+            foreach (var location in _locations)
+            {
+                if (location.Type == LocationType.National)
+                {
+                    if (location.ParentId != null)
+                    {
+                        throw new Exception("National location has parent id");
+                    }
+                        
+                }
+                else
+                {
+                    var parentId = location.ParentId;
+                    var parent = _locations.FindAll(l => l.LocationId == parentId);
+                    if (parent == null || parent.Count != 1)
+                    {
+                        throw new Exception($"Location {location.Code} does not have valid parent id");
+                    }
+                }                  
+            }
+
+            return true;
+        }
+    }
+}
---
+++ MedicalExaminers.ReferenceDataLoader.Tests/LocationsCheckerTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MedicalExaminer.Models;
+using MedicalExaminer.Models.Enums;
+using MedicalExaminer.ReferenceDataLoader.Loaders;
+using Xunit;
+
+namespace MedicalExaminers.ReferenceDataLoader.Tests
+{
+    public class LocationsCheckerTests
+    {
+        [Fact]
+        public void CheckParentIdsValid_AllAreValid()
+        {
+            ///Arrange
+            var national = new Location();
+            national.LocationId = "N1";
+            national.ParentId = null;
+            national.Type = LocationType.National;
+
+            var region1 = new Location();
+            region1.LocationId = "R1";
+            region1.ParentId = "N1";
+            region1.Type = LocationType.Region;
+
+            var trust1 = new Location();
+            trust1.LocationId = "T1";
+            trust1.ParentId = "R1";
+            trust1.Type = LocationType.Trust;
+
+            var site1 = new Location();
+            site1.LocationId = "S1";
+            site1.ParentId = "T1";
+            site1.Type = LocationType.Site;
+
+            var locations = new List<Location>();
+            locations.Add(national);
+            locations.Add(region1);
+            locations.Add(trust1);
+            locations.Add(site1);
+
+            var locationsChecker = new LocationsChecker(locations);
+
+
+            //Act
+            var result = locationsChecker.CheckParentIdsValid();
+
+            //Assert
+            Assert.True(result);
+        }
+    }
+}
