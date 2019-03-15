8ae934246d0d9006085ec51a6488427e9a9b0207
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-03-15 14:31:45 +0000

    Documentation for program class.

--- MedicalExaminer.API/Program.cs
+++ MedicalExaminer.API/Program.cs
@@ -3,13 +3,25 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace MedicalExaminer.API
 {
+    /// <summary>
+    /// Program hosting the API.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Main Entry Point.
+        /// </summary>
+        /// <param name="args">Argyments.</param>
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// Create the Web Host Builder.
+        /// </summary>
+        /// <param name="args">Pass through of arguments.</param>
+        /// <returns>Web Host Builder.</returns>
         public static IWebHostBuilder CreateWebHostBuilder(string[] args)
         {
             // Listen on any address so we can accept localhost and host.docker.internal from the python front end.
