c6b191e3e38bd4d841184b61582b59e76d6e3de6
DanukaH <48524567+DanukaH@users.noreply.github.com>
2019-03-28 13:41:48 +0000

    Update PreScrutinyEvent class

--- MedicalExaminer.Models/PreScrutinyEvent.cs
+++ MedicalExaminer.Models/PreScrutinyEvent.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
 using MedicalExaminer.Models.Enums;
 using Newtonsoft.Json;
 
@@ -10,7 +7,7 @@ namespace MedicalExaminer.Models
     public class PreScrutinyEvent
     {
         /// <summary>
-        /// Event Identication.
+        /// Event Identification.
         /// </summary>
         [JsonProperty(PropertyName = "event_id")]
         public string EventId { get; set; }
