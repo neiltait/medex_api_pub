099cc51a7bc67284e6026aeb4b406d8fe7e8175d
DavidJohnParsons <47145770+DavidJohnParsons@users.noreply.github.com>
2019-04-02 09:27:37 +0100

    Update OktaTokenCache.cs

--- MedicalExaminer.API/Services/Implementations/OktaTokenCache.cs
+++ MedicalExaminer.API/Services/Implementations/OktaTokenCache.cs
@@ -9,9 +9,34 @@ namespace MedicalExaminer.API.Services.Implementations
     /// <inheritdoc />
     public class OktaTokenCache : IOktaTokenCache
     {
+        /// <summary>
+        /// Time in seconds after which a cached token is deemed to have expired and is discarded
+        /// </summary>
+        /// <remarks>todo: make this configurable</remarks>
+        private const int expiryLimit = 600; 
+
+        protected Dictionary<string, ClaimsPrincipal> _claimsPrincipals;
+
+        protected struct TokenExpiry
+        {
+            public DateTime timestamp;
+            public string securityToken;
+        }
+
+        protected List<TokenExpiry> _tokenExpirys;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public OktaTokenCache()
+        {
+            _claimsPrincipals = new Dictionary<string, ClaimsPrincipal>();
+            _tokenExpirys = new List<TokenExpiry>();
+        }
         /// <inheritdoc />
         public ClaimsPrincipal RetrieveClaimsPrincipal(string securityToken)
         {
+            PurgeExpiredTokens();
             return null;
         }
 
@@ -19,7 +44,18 @@ namespace MedicalExaminer.API.Services.Implementations
         /// <inheritdoc />
         public void CacheClaimsPrincipal(string securityToken, ClaimsPrincipal claimsPrincipal, DateTime dateTime)
         {
+            
+        }
 
+        /// <summary>
+        /// Remove all tokens from dictionary that have expired
+        /// </summary>
+        private void PurgeExpiredTokens()
+        {
+            var timeNow = DateTime.Now;
+            var expiryCutOff = timeNow.AddSeconds(-expiryLimit);
+            var listExpired = _tokenExpirys.FindAll(t => t.timestamp < expiryCutOff).ToList();
+            _tokenExpirys.RemoveAll(t => t.timestamp < expiryCutOff);
         }
     }
 }
