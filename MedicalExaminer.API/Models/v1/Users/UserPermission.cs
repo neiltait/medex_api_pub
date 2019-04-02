d96805e9378658b720a437463882a56b0c9a04d7
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-04-02 09:30:51 +0100

    User permission

---
+++ MedicalExaminer.API/Models/v1/Users/UserPermission.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicalExaminer.API.Models.v1.Users
+{
+    public class UserPermission
+    {
+        public string PermissionId { get; set; }
+
+        public string LocationId { get; set; }
+
+        public int UserRole { get; set; }
+    }
+}
