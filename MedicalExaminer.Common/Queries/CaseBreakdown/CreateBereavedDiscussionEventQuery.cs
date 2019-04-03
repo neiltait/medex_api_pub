35323cadcd5622ced40796e93ec0539984cbd8fb
DanukaH <48524567+DanukaH@users.noreply.github.com>
2019-04-03 11:17:30 +0100

    BereavedDiscussionEventController, related request response classes and unit tests.

---
+++ MedicalExaminer.Common/Queries/CaseBreakdown/CreateBereavedDiscussionEventQuery.cs
@@ -0,0 +1,17 @@
+﻿using MedicalExaminer.Models;
+
+namespace MedicalExaminer.Common.Queries.CaseBreakdown
+{
+    public class CreateBereavedDiscussionEventQuery : IQuery<string>
+    {
+        public Models.BereavedDiscussionEvent BereavedDiscussionEvent { get; }
+
+        public string CaseId { get; }
+
+        public CreateBereavedDiscussionEventQuery(string caseId, BereavedDiscussionEvent bereavedDiscussionEvent)
+        {
+            CaseId = caseId;
+            BereavedDiscussionEvent = bereavedDiscussionEvent;
+        }
+    }
+}
