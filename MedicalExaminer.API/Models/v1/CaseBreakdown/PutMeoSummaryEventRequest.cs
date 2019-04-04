57144fbb827a6f369a698c8e831d564591dcb50e
DanukaH <48524567+DanukaH@users.noreply.github.com>
2019-04-04 15:14:51 +0100

    MEO Summary Event Dto, related classes and unit tests

---
+++ MedicalExaminer.API/Models/v1/CaseBreakdown/PutMeoSummaryEventRequest.cs
@@ -0,0 +1,36 @@
+﻿namespace MedicalExaminer.API.Models.v1.CaseBreakdown
+{
+    public class PutMeoSummaryEventRequest
+    {
+        /// <summary>
+        /// Event Identification.
+        /// </summary>
+        public string EventId { get; set; }
+
+        /// <summary>
+        /// User Identifier.
+        /// </summary>
+        public string UserId { get; set; }
+
+        //***************** uncomment after the generic event classes approved *********************
+        ///// <summary>
+        ///// Dictionary for the status (Draft or Final).
+        ///// </summary>
+        //public EventStatus EventStatus { get; set; }
+
+        ///// <summary>
+        ///// list of amendments for this item
+        ///// </summary>
+        //public IEnumerable<IEvent> Amendments { get; set; }
+
+        ///// <summary>
+        ///// the type of event this is
+        ///// </summary>
+        //public EventType EventType => EventType.BereavedDiscussion;
+
+        /// <summary>
+        /// Details of the MEO Summary.
+        /// </summary>
+        public string SummaryDetails { get; set; }
+    }
+}
---
+++ MedicalExaminer.Models/MeoSummaryEvent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace MedicalExaminer.Models
+{
+    public class MeoSummaryEvent
+    {
+        /// <summary>
+        /// Event Identification.
+        /// </summary>
+        [JsonProperty(PropertyName = "event_id")]
+        public string EventId { get; set; }
+
+        /// <summary>
+        /// User Identifier.
+        /// </summary>
+        [JsonProperty(PropertyName = "user_id")]
+        public string UserId { get; set; }
+
+        //***************** uncomment after the generic event classes approved *********************
+        ///// <summary>
+        ///// Dictionary for the status (Draft or Final).
+        ///// </summary>
+        //[JsonProperty(PropertyName = "event_status")]
+        //public EventStatus EventStatus { get; set; }
+
+        ///// <summary>
+        ///// list of ammendments for this item
+        ///// </summary>
+        //[JsonProperty(PropertyName = "amendments")]
+        //public IEnumerable<IEvent> Amendments { get; set; }
+
+        ///// <summary>
+        ///// the type of event this is
+        ///// </summary>
+        //[JsonProperty(PropertyName = "event_type")]
+        //public EventType EventType => EventType.BereavedDiscussion;
+
+        /// <summary>
+        /// Details of the MEO Summary.
+        /// </summary>
+        [JsonProperty(PropertyName = "summary_details")]
+        public string SummaryDetails { get; set; }
+    }
+}
