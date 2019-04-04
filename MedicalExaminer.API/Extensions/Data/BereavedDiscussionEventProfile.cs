5fef5020e3ad7aca4c3bb7f7c5b74b15ed3fb0b8
DanukaH <48524567+DanukaH@users.noreply.github.com>
2019-04-04 18:40:59 +0100

    Adding missing profiles

---
+++ MedicalExaminer.API/Extensions/Data/BereavedDiscussionEventProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using MedicalExaminer.API.Models.v1.CaseBreakdown;
+using MedicalExaminer.Models;
+
+namespace MedicalExaminer.API.Extensions.Data
+{
+    /// <summary>
+    ///     Examination Profile for AutoMapper
+    /// </summary>
+    public class BereavedDiscussionEventProfile : Profile
+    {
+        /// <summary>
+        ///     Initialise a new instance of the Examination AutoMapper Profile.
+        /// </summary>
+        public BereavedDiscussionEventProfile()
+        {
+            CreateMap<PutBereavedDiscussionEventRequest, BereavedDiscussionEvent>()
+                .ForMember(p => p.EventType, opt => opt.Ignore())
+                .ForMember(p => p.UserId, opt => opt.Ignore());
+        }
+    }
+}
