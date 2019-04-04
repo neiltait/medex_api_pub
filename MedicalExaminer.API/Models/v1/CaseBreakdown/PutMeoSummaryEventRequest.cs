6073b1525517c8137100bd8e81941c2ce2b636d5
mark <sharkeymark@gmail.com>
2019-04-04 16:13:15 +0100

    removed un-needed services, queries, controllers and tests
    updated the tests

--- MedicalExaminer.API/Models/v1/CaseBreakdown/PutMeoSummaryEventRequest.cs
+++ MedicalExaminer.API/Models/v1/CaseBreakdown/PutMeoSummaryEventRequest.cs
@@ -1,4 +1,6 @@
-﻿namespace MedicalExaminer.API.Models.v1.CaseBreakdown
+﻿using MedicalExaminer.Models.Enums;
+
+namespace MedicalExaminer.API.Models.v1.CaseBreakdown
 {
     public class PutMeoSummaryEventRequest
     {
@@ -12,21 +14,15 @@
         /// </summary>
         public string UserId { get; set; }
 
-        //***************** uncomment after the generic event classes approved *********************
         ///// <summary>
-        ///// Dictionary for the status (Draft or Final).
+        ///// bool for the status (Draft or Final).
         ///// </summary>
-        //public EventStatus EventStatus { get; set; }
+        public bool IsFinal { get; set; }
 
-        ///// <summary>
-        ///// list of amendments for this item
-        ///// </summary>
-        //public IEnumerable<IEvent> Amendments { get; set; }
-
-        ///// <summary>
-        ///// the type of event this is
-        ///// </summary>
-        //public EventType EventType => EventType.BereavedDiscussion;
+        /// <summary>
+        /// the type of event this is
+        /// </summary>
+        public EventType EventType => EventType.MeoSummary;
 
         /// <summary>
         /// Details of the MEO Summary.
--- MedicalExaminer.Models/MeoSummaryEvent.cs
+++ MedicalExaminer.Models/MeoSummaryEvent.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using MedicalExaminer.Models.Enums;
 using Newtonsoft.Json;
 
 namespace MedicalExaminer.Models
 {
-    public class MeoSummaryEvent
+    public class MeoSummaryEvent : IEvent
     {
         /// <summary>
         /// Event Identification.
@@ -19,24 +17,18 @@ namespace MedicalExaminer.Models
         [JsonProperty(PropertyName = "user_id")]
         public string UserId { get; set; }
 
-        //***************** uncomment after the generic event classes approved *********************
-        ///// <summary>
-        ///// Dictionary for the status (Draft or Final).
-        ///// </summary>
-        //[JsonProperty(PropertyName = "event_status")]
-        //public EventStatus EventStatus { get; set; }
-
-        ///// <summary>
-        ///// list of ammendments for this item
-        ///// </summary>
-        //[JsonProperty(PropertyName = "amendments")]
-        //public IEnumerable<IEvent> Amendments { get; set; }
-
-        ///// <summary>
-        ///// the type of event this is
-        ///// </summary>
-        //[JsonProperty(PropertyName = "event_type")]
-        //public EventType EventType => EventType.BereavedDiscussion;
+        
+        /// <summary>
+        /// draft = false, final = true
+        /// </summary>
+        [JsonProperty(PropertyName = "is_final")]
+        public bool IsFinal { get; set; }
+        
+        /// <summary>
+        /// the type of event this is
+        /// </summary>
+        [JsonProperty(PropertyName = "event_type")]
+        public EventType EventType => EventType.MeoSummary;
 
         /// <summary>
         /// Details of the MEO Summary.
