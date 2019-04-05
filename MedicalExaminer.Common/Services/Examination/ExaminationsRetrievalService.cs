9889b1eca8dcf1c817338209f34706ce0822f51f
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-04-05 14:07:45 +0100

    And one more.

--- MedicalExaminer.Common/Services/Examination/ExaminationsRetrievalService.cs
+++ MedicalExaminer.Common/Services/Examination/ExaminationsRetrievalService.cs
@@ -4,25 +4,31 @@ using System.Threading.Tasks;
 using MedicalExaminer.Common.ConnectionSettings;
 using MedicalExaminer.Common.Database;
 using MedicalExaminer.Common.Queries.Examination;
-using MedicalExaminer.Models;
 using MedicalExaminer.Models.Enums;
 
 namespace MedicalExaminer.Common.Services.Examination
 {
-    public class ExaminationsRetrievalService : IAsyncQueryHandler<ExaminationsRetrievalQuery, IEnumerable<Models.Examination>>
+    /// <summary>
+    /// Examinations Retrieval Service.
+    /// </summary>
+    public class ExaminationsRetrievalService : QueryHandler<ExaminationsRetrievalQuery, IEnumerable<Models.Examination>>
     {
-        private readonly IConnectionSettings _connectionSettings;
         private readonly ExaminationsQueryExpressionBuilder _examinationQueryBuilder;
-        private readonly IDatabaseAccess _databaseAccess;
 
+        /// <summary>
+        /// Initialise a new instance of <see cref="ExaminationsRetrievalService"/>.
+        /// </summary>
+        /// <param name="databaseAccess">Database Access.</param>
+        /// <param name="connectionSettings">Connection Settings.</param>
+        /// <param name="examinationQueryBuilder">Examination Query Builder.</param>
         public ExaminationsRetrievalService(IDatabaseAccess databaseAccess, IExaminationConnectionSettings connectionSettings, ExaminationsQueryExpressionBuilder examinationQueryBuilder)
+            : base(databaseAccess, connectionSettings)
         {
-            _databaseAccess = databaseAccess;
-            _connectionSettings = connectionSettings;
             _examinationQueryBuilder = examinationQueryBuilder;
         }
 
-        public Task<IEnumerable<Models.Examination>> Handle(ExaminationsRetrievalQuery param)
+        /// <inheritdoc/>
+        public override Task<IEnumerable<Models.Examination>> Handle(ExaminationsRetrievalQuery param)
         {
             if (param == null)
             {
@@ -34,11 +40,11 @@ namespace MedicalExaminer.Common.Services.Examination
             switch (param.FilterOrderBy)
             {
                 case ExaminationsOrderBy.Urgency:
-                    return _databaseAccess.GetItemsAsync(_connectionSettings, predicate, x => x.UrgencyScore);
+                    return GetItemsAsync(predicate, x => x.UrgencyScore);
                 case ExaminationsOrderBy.CaseCreated:
-                    return _databaseAccess.GetItemsAsync(_connectionSettings, predicate, x => x.CreatedAt);
+                    return GetItemsAsync(predicate, x => x.CreatedAt);
                 case null:
-                    return _databaseAccess.GetItemsAsync(_connectionSettings, predicate);
+                    return GetItemsAsync(predicate);
                 default:
                     throw new ArgumentOutOfRangeException(nameof(param.FilterOrderBy));
             }
