4ac83dd8528cde1a7ea64932efbff0c29ff0e4d9
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-04-05 17:20:53 +0100

    Added test coverage to role permissions.

---
+++ MedicalExaminer.Common.Tests/Authorization/RolePermissionsTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using MedicalExaminer.Common.Authorization;
+using MedicalExaminer.Models.Enums;
+using Moq;
+using Xunit;
+
+namespace MedicalExaminer.Common.Tests.Authorization
+{
+    /// <summary>
+    /// Role Permissions Tests.
+    /// </summary>
+    public class RolePermissionsTests
+    {
+        /// <summary>
+        /// Can Return True.
+        /// </summary>
+        [Fact]
+        public void Can_ReturnTrue()
+        {
+            // Arrange
+            var role = new TestRole();
+            var sut = new RolePermissions(new List<Role> { role });
+
+            // Act
+            var result = sut.Can(UserRoles.MedicalExaminer, Permission.AddEventToExamination);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        private class TestRole : Role
+        {
+            public TestRole()
+                : base(UserRoles.MedicalExaminer)
+            {
+                Grant(
+                    Permission.AddEventToExamination,
+                    Permission.AssignExaminationToMedicalExaminer);
+            }
+        }
+    }
+}
