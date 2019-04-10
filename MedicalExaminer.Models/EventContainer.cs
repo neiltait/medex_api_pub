b09ebf08c6642c9f06d22243369e8cb35b038ae8
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-04-10 14:45:20 +0100

    A bit more.

--- MedicalExaminer.Models/EventContainer.cs
+++ MedicalExaminer.Models/EventContainer.cs
@@ -5,9 +5,9 @@ namespace MedicalExaminer.Models
     public class OtherEventContainer : BaseEventContainter<OtherEvent>
     {
 
-        public override IEvent Latest { get; set; }
-        public override IList<IEvent> Drafts { get; set; } = new List<IEvent>();
-        public override IList<IEvent> History { get; set; } = new List<IEvent>();
+        public override OtherEvent Latest { get; set; }
+        public override IList<OtherEvent> Drafts { get; set; } = new List<OtherEvent>();
+        public override IList<OtherEvent> History { get; set; } = new List<OtherEvent>();
 
         public override void Add(OtherEvent theEvent)
         {
