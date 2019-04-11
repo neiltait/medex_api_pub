7ddbed159b2ce34eed8b8f9296abc0800a30c5cf
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-04-11 16:41:35 +0100

    Add service for user retrieval by role location. Needs tests.

---
+++ MedicalExaminer.Common/Services/User/UsersRetrievalByRoleLocationQueryService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using MedicalExaminer.Common.ConnectionSettings;
+using MedicalExaminer.Common.Database;
+using MedicalExaminer.Common.Queries.User;
+using MedicalExaminer.Models;
+
+namespace MedicalExaminer.Common.Services.User
+{
+    /// <summary>
+    /// Users Retrieval by Role Location Query Service.
+    /// </summary>
+    public class UsersRetrievalByRoleLocationQueryService : QueryHandler<UsersRetrievalByRoleLocationQuery, IEnumerable<MeUser>>
+    {
+        /// <summary>
+        /// Initialise a new instance of <see cref="UsersRetrievalByRoleLocationQueryService"/>.
+        /// </summary>
+        /// <param name="databaseAccess">Database Access.</param>
+        /// <param name="connectionSettings">Connection Settings.</param>
+        public UsersRetrievalByRoleLocationQueryService(
+            IDatabaseAccess databaseAccess,
+            IUserConnectionSettings connectionSettings)
+            : base(databaseAccess, connectionSettings)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override Task<IEnumerable<MeUser>> Handle(UsersRetrievalByRoleLocationQuery param)
+        {
+            if (param == null)
+            {
+                throw new ArgumentNullException(nameof(param));
+            }
+
+            Expression<Func<MeUser, bool>> predicate = mu =>
+                mu.Permissions.Any(p => param.Locations.Contains(p.LocationId) && p.UserRole == (int) param.Role);
+
+            return GetItemsAsync(predicate);
+        }
+    }
+}
