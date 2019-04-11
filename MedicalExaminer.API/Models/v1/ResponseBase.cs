60385c4214ffc81a16514571bf526e796ebd8849
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-04-11 16:41:04 +0100

    Add lookups to response base.

--- MedicalExaminer.API/Models/v1/ResponseBase.cs
+++ MedicalExaminer.API/Models/v1/ResponseBase.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace MedicalExaminer.API.Models.v1
@@ -12,6 +13,11 @@ namespace MedicalExaminer.API.Models.v1
         ///     Get the Errors.
         /// </summary>
         public IDictionary<string, ICollection<string>> Errors { get; } = new Dictionary<string, ICollection<string>>();
+
+        /// <summary>
+        /// Lookups.
+        /// </summary>
+        public IDictionary<string, IDictionary<string, string>> Lookups { get; private set; }
 
         /// <summary>
         ///     Gets a value indicating whether this response is successful.
@@ -34,6 +40,26 @@ namespace MedicalExaminer.API.Models.v1
             }
 
             messages.Add(message);
+        }
+
+        /// <summary>
+        /// Add a lookup to the response.
+        /// </summary>
+        /// <param name="key">The key for the lookup</param>
+        /// <param name="lookup">A dictionary of lookups</param>
+        public void AddLookup(string key, IDictionary<string, string> lookup)
+        {
+            if (Lookups == null)
+            {
+                Lookups = new Dictionary<string, IDictionary<string, string>>();
+            }
+
+            if (Lookups.ContainsKey(key))
+            {
+                throw new ArgumentException("Key already added");
+            }
+
+            Lookups[key] = lookup;
         }
     }
 }
