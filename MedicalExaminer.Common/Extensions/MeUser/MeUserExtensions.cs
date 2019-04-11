217befa935d56b3635121d7c9fd79ccb302f0682
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-04-11 16:41:16 +0100

    Add full name extension to users.

---
+++ MedicalExaminer.Common/Extensions/MeUser/MeUserExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedicalExaminer.Common.Extensions.MeUser
+{
+    public static class MeUserExtensions
+    {
+        /// <summary>
+        /// Get the full name, combining last name and first name
+        /// </summary>
+        /// <param name="meUser">User object.</param>
+        /// <returns>Full name string.</returns>
+        public static string FullName(this Models.MeUser meUser)
+        {
+            // TODO: Do we have a standard way of doing this? Can we assume this format? Does it need to be last name first?
+            return $"{meUser.FirstName} {meUser.LastName}";
+        }
+    }
+}
