a3feefa44d08209ac0be1ad921698c92ca6dab56
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-04-18 09:36:34 +0100

    Multiple location path queries keyed by starting location.

---
+++ MedicalExaminer.API.Tests/Services/Location/LocationsParentsQueryServiceTests.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MedicalExaminer.Common.ConnectionSettings;
+using MedicalExaminer.Common.Database;
+using MedicalExaminer.Common.Queries.Location;
+using MedicalExaminer.Common.Services.Location;
+using MedicalExaminer.Models.Enums;
+using Moq;
+using Xunit;
+
+namespace MedicalExaminer.API.Tests.Services.Location
+{
+    public class LocationsParentsQueryServiceTests : ServiceTestsBase<
+        LocationsParentsQuery,
+        LocationConnectionSettings,
+        IDictionary<string,IEnumerable<MedicalExaminer.Models.Location>>,
+        MedicalExaminer.Models.Location,
+        LocationsParentsQueryService>
+    {
+        [Fact]
+        public async Task Handle_ReturnsAllParents_FromMultipleSites()
+        {
+            // Arrange
+            var query = new LocationsParentsQuery(new[] { "site1", "site2" });
+
+            // Act
+            var results = await Service.Handle(query);
+
+            // Assert
+            results.Should().NotBeNull();
+            results.Count.Should().Be(2);
+
+            results["site1"].Count().Should().Be(4);
+            results["site1"].Count(l => l.Type == LocationType.National).Should().Be(1);
+            results["site1"].Count(l => l.Type == LocationType.Region).Should().Be(1);
+            results["site1"].Count(l => l.Type == LocationType.Trust).Should().Be(1);
+            results["site1"].Count(l => l.Type == LocationType.Site).Should().Be(1);
+
+            results["site2"].Count().Should().Be(4);
+            results["site2"].Count(l => l.Type == LocationType.National).Should().Be(1);
+            results["site2"].Count(l => l.Type == LocationType.Region).Should().Be(1);
+            results["site2"].Count(l => l.Type == LocationType.Trust).Should().Be(1);
+            results["site2"].Count(l => l.Type == LocationType.Site).Should().Be(1);
+        }
+
+        [Fact]
+        public async Task Handle_ReturnsAllParents_FromSitesSharingPath()
+        {
+            // Arrange
+            var query = new LocationsParentsQuery(new[] { "site1", "trust1" });
+
+            // Act
+            var results = await Service.Handle(query);
+
+            // Assert
+            results.Should().NotBeNull();
+            results.Count.Should().Be(2);
+
+            results["site1"].Count().Should().Be(4);
+            results["site1"].Count(l => l.Type == LocationType.National).Should().Be(1);
+            results["site1"].Count(l => l.Type == LocationType.Region).Should().Be(1);
+            results["site1"].Count(l => l.Type == LocationType.Trust).Should().Be(1);
+            results["site1"].Count(l => l.Type == LocationType.Site).Should().Be(1);
+
+            results["trust1"].Count().Should().Be(3);
+            results["trust1"].Count(l => l.Type == LocationType.National).Should().Be(1);
+            results["trust1"].Count(l => l.Type == LocationType.Region).Should().Be(1);
+            results["trust1"].Count(l => l.Type == LocationType.Trust).Should().Be(1);
+        }
+
+        [Fact]
+        public async Task Handle_ReturnsAllParents_FromSitesWithSameLocation()
+        {
+            // Arrange
+            var query = new LocationsParentsQuery(new[] { "site1", "site1" });
+
+            // Act
+            var results = await Service.Handle(query);
+
+            // Assert
+            results.Should().NotBeNull();
+            results.Count.Should().Be(1);
+
+            results["site1"].Count().Should().Be(4);
+            results["site1"].Count(l => l.Type == LocationType.National).Should().Be(1);
+            results["site1"].Count(l => l.Type == LocationType.Region).Should().Be(1);
+            results["site1"].Count(l => l.Type == LocationType.Trust).Should().Be(1);
+            results["site1"].Count(l => l.Type == LocationType.Site).Should().Be(1);
+        }
+
+        [Fact]
+        public async Task Handle_ReturnsAllParents_FromSitesSharingEntirePath()
+        {
+            // Arrange
+            var query = new LocationsParentsQuery(new[] { "site1", "trust1", "region1", "national1" });
+
+            // Act
+            var results = await Service.Handle(query);
+
+            // Assert
+            results.Should().NotBeNull();
+            results.Count.Should().Be(4);
+
+            results["site1"].Count().Should().Be(4);
+            results["site1"].Count(l => l.Type == LocationType.National).Should().Be(1);
+            results["site1"].Count(l => l.Type == LocationType.Region).Should().Be(1);
+            results["site1"].Count(l => l.Type == LocationType.Trust).Should().Be(1);
+            results["site1"].Count(l => l.Type == LocationType.Site).Should().Be(1);
+
+            results["trust1"].Count().Should().Be(3);
+            results["trust1"].Count(l => l.Type == LocationType.National).Should().Be(1);
+            results["trust1"].Count(l => l.Type == LocationType.Region).Should().Be(1);
+            results["trust1"].Count(l => l.Type == LocationType.Trust).Should().Be(1);
+
+            results["region1"].Count().Should().Be(2);
+            results["region1"].Count(l => l.Type == LocationType.National).Should().Be(1);
+            results["region1"].Count(l => l.Type == LocationType.Region).Should().Be(1);
+
+            results["national1"].Count().Should().Be(1);
+            results["national1"].Count(l => l.Type == LocationType.National).Should().Be(1);
+        }
+
+        [Fact]
+        public async Task Handle_ReturnsAllParents_WithoutSharing()
+        {
+            // Arrange
+            var query = new LocationsParentsQuery(new[] { "site1", "trust2", "region3", "national4" });
+
+            // Act
+            var results = await Service.Handle(query);
+
+            // Assert
+            results.Should().NotBeNull();
+            results.Count.Should().Be(4);
+
+            results["site1"].Count().Should().Be(4);
+            results["site1"].Count(l => l.Type == LocationType.National).Should().Be(1);
+            results["site1"].Count(l => l.Type == LocationType.Region).Should().Be(1);
+            results["site1"].Count(l => l.Type == LocationType.Trust).Should().Be(1);
+            results["site1"].Count(l => l.Type == LocationType.Site).Should().Be(1);
+
+            results["trust2"].Count().Should().Be(3);
+            results["trust2"].Count(l => l.Type == LocationType.National).Should().Be(1);
+            results["trust2"].Count(l => l.Type == LocationType.Region).Should().Be(1);
+            results["trust2"].Count(l => l.Type == LocationType.Trust).Should().Be(1);
+
+            results["region3"].Count().Should().Be(2);
+            results["region3"].Count(l => l.Type == LocationType.National).Should().Be(1);
+            results["region3"].Count(l => l.Type == LocationType.Region).Should().Be(1);
+
+            results["national4"].Count().Should().Be(1);
+            results["national4"].Count(l => l.Type == LocationType.National).Should().Be(1);
+        }
+
+        protected override MedicalExaminer.Models.Location[] GetExamples()
+        {
+            return Enumerable.Range(1, 4).SelectMany(i => new[]
+            {
+                new MedicalExaminer.Models.Location()
+                {
+                    LocationId = $"national{i}",
+                    ParentId = null,
+                    Type = LocationType.National,
+                },
+                new MedicalExaminer.Models.Location()
+                {
+                    LocationId = $"region{i}",
+                    ParentId = $"national{i}",
+                    Type = LocationType.Region,
+                },
+                new MedicalExaminer.Models.Location()
+                {
+                    LocationId = $"trust{i}",
+                    ParentId = $"region{i}",
+                    Type = LocationType.Trust,
+                },
+                new MedicalExaminer.Models.Location()
+                {
+                    LocationId = $"site{i}",
+                    ParentId = $"trust{i}",
+                    Type = LocationType.Site,
+                },
+            }).ToArray();
+        }
+    }
+}
---
+++ MedicalExaminer.Common/Services/Location/LocationsParentsQueryService.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MedicalExaminer.Common.ConnectionSettings;
+using MedicalExaminer.Common.Database;
+using MedicalExaminer.Common.Queries.Location;
+
+namespace MedicalExaminer.Common.Services.Location
+{
+    /// <summary>
+    /// Locations Parents Query Service.
+    /// </summary>
+    public class LocationsParentsQueryService : QueryHandler<LocationsParentsQuery, IDictionary<string,IEnumerable<Models.Location>>>
+    {
+        /// <summary>
+        /// Maximum number of "recursive" loops before exiting.
+        /// </summary>
+        /// <remarks>Shouldn't be possible if locations are automatically configured, but better not left to chance.</remarks>
+        private const int MaxLoopIterations = 10;
+
+        /// <summary>
+        /// Initialise a new instance of <see cref="LocationParentsQueryService"/>.
+        /// </summary>
+        /// <param name="databaseAccess">Database Access.</param>
+        /// <param name="connectionSettings">Connection Settings</param>
+        public LocationsParentsQueryService(
+            IDatabaseAccess databaseAccess,
+            ILocationConnectionSettings connectionSettings)
+            : base(databaseAccess, connectionSettings)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override async Task<IDictionary<string,IEnumerable<Models.Location>>> Handle(LocationsParentsQuery param)
+        {
+            if (param == null)
+            {
+                throw new ArgumentNullException(nameof(param));
+            }
+
+            // Initialise with the key and value the same
+            var locationIds = param.LocationIds.ToList();
+
+            var maxLoops = MaxLoopIterations;
+            var result = param.LocationIds.Distinct().ToDictionary(
+                k => k,
+                v => new List<Models.Location>());
+
+            while (locationIds.Any() && (maxLoops--) > 0)
+            {
+                var ids = locationIds.ToList();
+
+                var items = (await DatabaseAccess.GetItemsAsync(
+                    ConnectionSettings,
+                    (Models.Location location) => ids.Contains(location.LocationId))).ToList();
+
+                if (!items.Any())
+                {
+                    break;
+                }
+
+                foreach (var item in items)
+                {
+                    var keys = result
+                        .Where(kvp =>
+                            (!kvp.Value.Any() && kvp.Key == item.LocationId)
+                            || (kvp.Value.Any() && kvp.Value.Last().ParentId == item.LocationId))
+                        .Select(kvp => kvp.Key);
+
+                    foreach (var key in keys)
+                    {
+                        result[key].Add(item);
+                    }
+                }
+
+                locationIds = result.Select(r => r.Value.Last().ParentId).ToList();
+            }
+
+            // Stop the value being a list
+            return result.ToDictionary(
+                k => k.Key,
+                v => v.Value.AsEnumerable());
+        }
+    }
+}
