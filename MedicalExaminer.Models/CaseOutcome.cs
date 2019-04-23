33496f8312e69db28a97b9af6e55b799764fd9b9
DanukaH <48524567+DanukaH@users.noreply.github.com>
2019-04-23 15:23:58 +0100

    Create CaseOutcome.cs

---
+++ MedicalExaminer.Models/CaseOutcome.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MedicalExaminer.Models.Enums;
+using Newtonsoft.Json;
+
+namespace MedicalExaminer.Models
+{
+    public class CaseOutcome
+    {
+        /// <summary>
+        /// Whether the MCCD is Issued.
+        /// </summary>
+        [JsonProperty(PropertyName = "mccd_issued")]
+        public bool MccdIssued { get; set; }
+
+        /// <summary>
+        /// Whether the Cremation form is completed.
+        /// </summary>
+        [JsonProperty(PropertyName = "cremation_form_status")]
+        public CremationFormStatus CremationFormStatus { get; set; }
+
+        /// <summary>
+        /// Whether the GP is notified.
+        /// </summary>
+        [JsonProperty(PropertyName = "gp_notified")]
+        public GPNotified GpNotified { get; set; }
+    }
+}
