f06f1ee411346d2cbbd41a260fb9e08fc72dad1b
mark <sharkeymark@gmail.com>
2019-04-23 11:26:10 +0100

    Update MedicalTeamUpdateService.cs

--- MedicalExaminer.Common/Services/MedicalTeam/MedicalTeamUpdateService.cs
+++ MedicalExaminer.Common/Services/MedicalTeam/MedicalTeamUpdateService.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using MedicalExaminer.Common.ConnectionSettings;
 using MedicalExaminer.Common.Database;
+using MedicalExaminer.Common.Queries.User;
 using MedicalExaminer.Models;
 
 namespace MedicalExaminer.Common.Services.MedicalTeam
@@ -10,13 +11,16 @@ namespace MedicalExaminer.Common.Services.MedicalTeam
     {
         private readonly IConnectionSettings _connectionSettings;
         private readonly IDatabaseAccess _databaseAccess;
+        private readonly IAsyncQueryHandler<UserRetrievalByIdQuery, MeUser> _userService;
 
         public MedicalTeamUpdateService(
             IDatabaseAccess databaseAccess,
-            IExaminationConnectionSettings connectionSettings)
+            IExaminationConnectionSettings connectionSettings,
+            IAsyncQueryHandler<UserRetrievalByIdQuery, MeUser> userService)
         {
             _databaseAccess = databaseAccess;
             _connectionSettings = connectionSettings;
+            _userService = userService;
         }
 
         public async Task<Models.Examination> Handle(Models.Examination examination)
@@ -25,6 +29,12 @@ namespace MedicalExaminer.Common.Services.MedicalTeam
             {
                 throw new ArgumentNullException(nameof(examination));
             }
+
+            var me = await _userService.Handle(new UserRetrievalByIdQuery(examination.MedicalTeam.MedicalExaminerUserId));
+            var meo = await _userService.Handle(new UserRetrievalByIdQuery(examination.MedicalTeam.MedicalExaminerOfficerUserId));
+
+            examination.MedicalTeam.MedicalExaminerFullName = me.FullName();
+            examination.MedicalTeam.MedicalExaminerOfficerFullName = meo.FullName();
 
             examination.UpdateCaseUrgencyScore();
 
