db8185a124a6d32cc727b6a432ccecdce5a1f3c5
DanukaH <48524567+DanukaH@users.noreply.github.com>
2019-04-25 09:58:51 +0100

    Update CaseOutcomeController.cs

--- MedicalExaminer.API/Controllers/CaseOutcomeController.cs
+++ MedicalExaminer.API/Controllers/CaseOutcomeController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using MedicalExaminer.API.Models.v1.CaseOutcome;
 using MedicalExaminer.Common.Loggers;
+using MedicalExaminer.Common.Queries.CaseOutcome;
 using MedicalExaminer.Common.Queries.Examination;
 using MedicalExaminer.Common.Queries.User;
 using MedicalExaminer.Common.Services;
@@ -18,29 +19,35 @@ namespace MedicalExaminer.API.Controllers
     [Authorize]
     public class CaseOutcomeController : AuthenticatedBaseController
     {
-        private IAsyncQueryHandler<ExaminationRetrievalQuery, Examination> _examinationRetrievalService;
+        private IAsyncQueryHandler<ConfirmationOfScrutinyQuery, Examination> _confirmationOfScrutinyService;
 
         public CaseOutcomeController(
             IMELogger logger,
             IMapper mapper,
-            IAsyncQueryHandler<ExaminationRetrievalQuery, Examination> examinationRetrievalService,
+            IAsyncQueryHandler<ConfirmationOfScrutinyQuery, Examination> confirmationOfScrutinyService,
             IAsyncQueryHandler<UserRetrievalByEmailQuery, MeUser> usersRetrievalByEmailService)
             : base(logger, mapper, usersRetrievalByEmailService)
         {
-            _examinationRetrievalService = examinationRetrievalService;
+            _confirmationOfScrutinyService = confirmationOfScrutinyService;
         }
 
+        /// <summary>
+        /// Confirmation Of Scrutiny
+        /// </summary>
+        /// <param name="examinationId"></param>
+        /// <returns></returns>
         [HttpPut]
         [Route("confirmation_of_scrutiny")]
         public async Task<ActionResult<PutConfirmationOfScrutinyResponse>> PutConfirmationOfScrutiny(string examinationId)
         {
             var user = await CurrentUser();
-            //var examination = await _examinationRetrievalService.Handle(new ExaminationRetrievalQuery(examinationId, user));
-            var confirmationOfScrutinyQuery = new ConfirmationOfScrutinyQuery(examinationId, user);
+
+            // var confirmationOfScrutinyQuery = new ConfirmationOfScrutinyQuery(examinationId, user);
+            var result = await _confirmationOfScrutinyService.Handle(new ConfirmationOfScrutinyQuery(examinationId, user));
 
             return Ok(new PutConfirmationOfScrutinyResponse()
             {
-                ScrutinyConfirmedOn = DateTime.Now
+                ScrutinyConfirmedOn = result.ConfirmationOfScrutinyCompletedAt
             });
         }
 
