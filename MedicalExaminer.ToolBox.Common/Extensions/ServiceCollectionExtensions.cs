aaf8cef90c472d3782ed9d867819dde11563fe25
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-04-26 14:35:33 +0100

    Toolbox common and MVC project.

---
+++ MedicalExaminer.ToolBox.Common/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper.Configuration;
+using Cosmonaut;
+using Cosmonaut.Extensions.Microsoft.DependencyInjection;
+using MedicalExaminer.Common.ConnectionSettings;
+using MedicalExaminer.Common.Database;
+using MedicalExaminer.Common.Queries.User;
+using MedicalExaminer.Common.Services;
+using MedicalExaminer.Common.Services.Examination;
+using MedicalExaminer.Common.Services.User;
+using MedicalExaminer.Models;
+using MedicalExaminer.ToolBox.Common.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace MedicalExaminer.ToolBox.Common.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection ConfigureToolBox(this IServiceCollection services, Microsoft.Extensions.Configuration.IConfiguration configuration)
+        {
+            var cosmosSettings = new CosmosStoreSettings(
+                configuration["CosmosDB:DatabaseId"],
+                configuration["CosmosDB:URL"],
+                configuration["CosmosDB:PrimaryKey"]);
+
+            services.AddCosmosStore<MeUser>(cosmosSettings, "Users");
+            services.AddCosmosStore<Location>(cosmosSettings, "Locations");
+
+
+            services.AddScoped<IDocumentClientFactory, DocumentClientFactory>();
+            services.AddScoped<IDatabaseAccess, DatabaseAccess>();
+
+            services.AddScoped<IUserConnectionSettings>(s => new UserConnectionSettings(
+                new Uri(configuration["CosmosDB:URL"]),
+                configuration["CosmosDB:PrimaryKey"],
+                configuration["CosmosDB:DatabaseId"]));
+
+            services.AddScoped<IAsyncQueryHandler<UserRetrievalByIdQuery, MeUser>, UserRetrievalByIdService>();
+            services.AddScoped<IAsyncQueryHandler<UserUpdateQuery, MeUser>, UserUpdateService>();
+
+            services.AddScoped<IAsyncQueryHandler<UsersRetrievalQuery, IEnumerable<MeUser>>, UsersRetrievalService>();
+
+            services.AddScoped<ImpersonateUserService>();
+            services.AddScoped<GenerateConfigurationService>();
+
+            return services;
+        }
+    }
+}
---
+++ MedicalExaminer.ToolBox.MVC/Controllers/GenerateController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MedicalExaminer.ToolBox.Common.Dtos.Generate;
+using MedicalExaminer.ToolBox.Common.Services;
+using MedicalExaminer.ToolBox.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedicalExaminer.ToolBox.MVC.Controllers
+{
+    public class GenerateController : Controller
+    {
+        private GenerateConfigurationService _generateConfigurationService;
+
+        public GenerateController(GenerateConfigurationService generateConfigurationService)
+        {
+            _generateConfigurationService = generateConfigurationService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Configure()
+        {
+            return View(new GenerateViewModel());
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<GenerateResultViewModel>> Configure(GenerateViewModel viewModel)
+        {
+            await _generateConfigurationService.Generate(new GenerateConfiguration()
+            {
+                NumberOfRegions = viewModel.NumberOfRegions,
+                NumberOfTrusts = viewModel.NumberOfTrusts,
+                NumberOfSites = viewModel.NumberOfSites,
+
+                NumberOfMedicalExaminersPerSite = viewModel.NumberOfMedicalExaminersPerSite,
+                NumberOfMedicalExaminerOfficersPerSite = viewModel.NumberOfMedicalExaminerOfficersPerSite,
+
+                NumberOfMedicalExaminersPerTrust = viewModel.NumberOfMedicalExaminersPerTrust,
+                NumberOfMedicalExaminerOfficersPerTrust = viewModel.NumberOfMedicalExaminerOfficersPerTrust,
+
+                NumberOfMedicalExaminersPerRegion = viewModel.NumberOfMedicalExaminersPerRegion,
+                NumberOfMedicalExaminerOfficersPerRegion = viewModel.NumberOfMedicalExaminerOfficersPerRegion,
+                NumberOfServiceAdministratorsPerRegion = viewModel.NumberOfServiceAdministratorsPerRegion,
+
+                NumberOfServiceOwnersPerNational = viewModel.NumberOfServiceOwnersPerNational,
+            });
+
+            return  View("ConfigurePost", new GenerateResultViewModel()
+            {
+                Success = false,
+            });
+        }
+    }
+}
