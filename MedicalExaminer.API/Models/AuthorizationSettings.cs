2714cc3e05eb4e7778a62b0884a4e71cb77575ea
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-04-26 15:36:38 +0100

    Added new setting for authorization to disable it.

---
+++ MedicalExaminer.API/Models/AuthorizationSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicalExaminer.API.Models
+{
+    /// <summary>
+    /// Authorization Settings.
+    /// </summary>
+    public class AuthorizationSettings
+    {
+        /// <summary>
+        /// Disable.
+        /// </summary>
+        public bool Disable { get; set; }
+    }
+}
