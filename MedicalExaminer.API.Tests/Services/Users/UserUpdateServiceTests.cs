13ac826f40dee795d9c81c6f746b67cb2bbadc2c
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-04-30 01:10:56 +0100

    Missing comments and tests for update user service.

---
+++ MedicalExaminer.API.Tests/Services/Users/UserUpdateServiceTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MedicalExaminer.Common.ConnectionSettings;
+using MedicalExaminer.Common.Queries.User;
+using MedicalExaminer.Common.Services.User;
+using MedicalExaminer.Models;
+using Xunit;
+
+namespace MedicalExaminer.API.Tests.Services.Users
+{
+    public class UserUpdateServiceTests : ServiceTestsBase<
+        UserUpdateQuery,
+        UserConnectionSettings,
+        MeUser,
+        MeUser,
+        UserUpdateService>
+    {
+        [Fact]
+        public async Task Handle_UpdateUser()
+        {
+            // Arrange
+            var expectedUserId = "expectedUserId";
+            var expectedEmail = "email1";
+            var expectedUser = new MeUser()
+            {
+                UserId = expectedUserId,
+                Email = expectedEmail,
+            };
+
+            var query = new UserUpdateQuery(expectedUser);
+
+            // Act
+            var result = await Service.Handle(query);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.UserId.Should().NotBeNull();
+            result.Email.Should().Be(expectedEmail);
+        }
+
+        [Fact]
+        public async Task Handle_UpdateUser_ThatDoesntExist_ThrowsException()
+        {
+            // Arrange
+            var expectedUserId = "userId3";
+            var expectedEmail = "email1";
+            var expectedUser = new MeUser()
+            {
+                UserId = expectedUserId,
+                Email = expectedEmail,
+            };
+
+            var query = new UserUpdateQuery(expectedUser);
+
+            // Act
+            Func<Task> act = async () => await Service.Handle(query);
+
+            // Assert
+            act.Should().Throw<InvalidOperationException>();
+        }
+
+        protected override MeUser[] GetExamples()
+        {
+            return new[]
+            {
+                new MeUser()
+                {
+                    UserId = "expectedUserId",
+                    Email = "email1",
+                },
+                new MeUser()
+                {
+                    UserId = "userId2",
+                    Email = "email2",
+                },
+            };
+        }
+    }
+}
