2fbafed42a926a1840373df08c518bedb1d88395
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-05-03 11:07:24 +0100

    Added missing tests.

--- MedicalExaminer.Common.Tests/Extensions/MeUser/MeUserExtensionsTests.cs
+++ MedicalExaminer.Common.Tests/Extensions/MeUser/MeUserExtensionsTests.cs
@@ -8,10 +8,130 @@ using Xunit;
 namespace MedicalExaminer.Common.Tests.Extensions.MeUser
 {
     /// <summary>
-    /// A class class.
+    /// Me User Extension Tests.
     /// </summary>
     public class MeUserExtensionsTests
     {
+        /// <summary>
+        /// Role Returns Best Role for a User based on the Enum ordering.
+        /// </summary>
+        [Fact]
+        public void Role_ReturnsBestRoleForUser_BasedOnEnumOrdering()
+        {
+            // Arrange
+            var expectedRole = UserRoles.ServiceOwner;
+            var user = new Models.MeUser()
+            {
+                Permissions = new List<MEUserPermission>()
+                {
+                    new MEUserPermission()
+                    {
+                        LocationId = "site1",
+                        UserRole = UserRoles.MedicalExaminerOfficer
+                    },
+                    new MEUserPermission()
+                    {
+                        LocationId = "trust1",
+                        UserRole = UserRoles.MedicalExaminer
+                    },
+                    new MEUserPermission()
+                    {
+                        LocationId = "national1",
+                        UserRole = expectedRole
+                    }
+                }
+            };
+
+            // Act
+            var role = user.Role();
+
+            // Assert
+            role.Should().Be(expectedRole);
+        }
+
+        /// <summary>
+        /// Has Role for Examination returns true when user has required role.
+        /// </summary>
+        [Fact]
+        public void HasRoleForExamination_ReturnsTrue_WhenUserHasRequiredRole()
+        {
+            // Arrange
+            var user = new Models.MeUser()
+            {
+                Permissions = new List<MEUserPermission>()
+                {
+                    new MEUserPermission()
+                    {
+                        LocationId = "site1",
+                        UserRole = UserRoles.MedicalExaminerOfficer
+                    },
+                    new MEUserPermission()
+                    {
+                        LocationId = "trust1",
+                        UserRole = UserRoles.MedicalExaminer
+                    },
+                    new MEUserPermission()
+                    {
+                        LocationId = "national1",
+                        UserRole = UserRoles.ServiceAdministrator,
+                    }
+                }
+            };
+
+            var examination = new Models.Examination()
+            {
+                NationalLocationId = "national1",
+                RegionLocationId = "region1",
+                TrustLocationId = "trust1",
+                SiteLocationId = "site1"
+            };
+
+            // Act
+            var hasRole = user.HasRoleForExamination(examination, UserRoles.MedicalExaminerOfficer);
+
+            // Assert
+            hasRole.Should().BeTrue();
+        }
+
+        /// <summary>
+        /// Has Role for Examination returns false when user does not have required role.
+        /// </summary>
+        [Fact]
+        public void HasRoleForExamination_ReturnsFalse_WhenUserDoesNotHaveRequiredRole()
+        {
+            // Arrange
+            var user = new Models.MeUser()
+            {
+                Permissions = new List<MEUserPermission>()
+                {
+                    new MEUserPermission()
+                    {
+                        LocationId = "trust1",
+                        UserRole = UserRoles.MedicalExaminer
+                    },
+                    new MEUserPermission()
+                    {
+                        LocationId = "national1",
+                        UserRole = UserRoles.ServiceAdministrator,
+                    }
+                }
+            };
+
+            var examination = new Models.Examination()
+            {
+                NationalLocationId = "national1",
+                RegionLocationId = "region1",
+                TrustLocationId = "trust1",
+                SiteLocationId = "site1"
+            };
+
+            // Act
+            var hasRole = user.HasRoleForExamination(examination, UserRoles.MedicalExaminerOfficer);
+
+            // Assert
+            hasRole.Should().BeFalse();
+        }
+
         /// <summary>
         /// A test.
         /// </summary>
@@ -61,7 +181,7 @@ namespace MedicalExaminer.Common.Tests.Extensions.MeUser
         /// A nother test.
         /// </summary>
         [Fact]
-        public void UsersExaminationRole_EmPtyRequiredRoles_Returns_Null()
+        public void UsersExaminationRole_EmptyRequiredRoles_Returns_Null()
         {
             // Arrange
             var expectedRole = UserRoles.MedicalExaminer;
@@ -83,13 +203,6 @@ namespace MedicalExaminer.Common.Tests.Extensions.MeUser
             };
 
             List<UserRoles> requiredRoles = null;
-            var examination = new Models.Examination()
-            {
-                NationalLocationId = "national1",
-                RegionLocationId = "region1",
-                TrustLocationId = "trust1",
-                SiteLocationId = "site1"
-            };
 
             // Act
             var role = user.UsersExaminationRole(requiredRoles);
@@ -129,14 +242,6 @@ namespace MedicalExaminer.Common.Tests.Extensions.MeUser
                 UserRoles.MedicalExaminerOfficer
             };
 
-            var examination = new Models.Examination()
-            {
-                NationalLocationId = "national1",
-                RegionLocationId = "region1",
-                TrustLocationId = "trust1",
-                SiteLocationId = "site1"
-            };
-
             // Act
             var role = user.UsersExaminationRole(requiredRoles);
 
@@ -169,14 +274,6 @@ namespace MedicalExaminer.Common.Tests.Extensions.MeUser
                 UserRoles.MedicalExaminerOfficer
             };
 
-            var examination = new Models.Examination()
-            {
-                NationalLocationId = "national1",
-                RegionLocationId = "region1",
-                TrustLocationId = "trust1",
-                SiteLocationId = "site1"
-            };
-
             // Act
             var role = user.UsersExaminationRole(requiredRoles);
 
@@ -200,14 +297,6 @@ namespace MedicalExaminer.Common.Tests.Extensions.MeUser
             {
                 UserRoles.MedicalExaminer,
                 UserRoles.MedicalExaminerOfficer
-            };
-
-            var examination = new Models.Examination()
-            {
-                NationalLocationId = "national1",
-                RegionLocationId = "region1",
-                TrustLocationId = "trust1",
-                SiteLocationId = "site1"
             };
 
             // Act
