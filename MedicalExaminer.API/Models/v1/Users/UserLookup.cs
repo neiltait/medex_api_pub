e8bb57704a72484c86d581d55987b1415f2ed056
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-05-03 09:30:58 +0100

    Added new user lookup class with only full name and id.

---
+++ MedicalExaminer.API/Models/v1/Users/UserLookup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicalExaminer.API.Models.v1.Users
+{
+    /// <summary>
+    /// User Lookup
+    /// </summary>
+    /// <remarks>User fields when displayed in lookup lists.</remarks>
+    public class UserLookup
+    {
+        /// <summary>
+        /// The User identifier.
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// Display Name
+        /// </summary>
+        public string FullName { get; set; }
+    }
+}
