56b460bebb79510e2c1df003ae4c2ca71522abb1
mark <sharkeymark@gmail.com>
2019-05-09 12:12:55 +0100

    audit log initial check in

---
+++ MedicalExaminer.Common/Database/AuditEntry.cs
@@ -0,0 +1,16 @@
+﻿using MedicalExaminer.Models;
+
+namespace MedicalExaminer.Common.Database
+{
+    public class AuditEntry<T>
+    {
+        public string id { get; set; }
+
+        public T Entry { get; set; }
+
+        public AuditEntry(T item)
+        {
+            Entry = item;
+        }
+    }
+}
