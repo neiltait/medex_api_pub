14eb276246f06617ef85080272691e57aba9c09b
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-05-09 13:29:48 +0100

    Some test coverage over the config.

--- MedicalExaminer.BackgroundServices.Tests/ScheduledServiceEveryDayAtSetTimeTests.cs
+++ MedicalExaminer.BackgroundServices.Tests/ScheduledServiceEveryDayAtSetTimeTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 using FluentAssertions;
 using Xunit;
@@ -9,18 +10,61 @@ namespace MedicalExaminer.BackgroundServices.Tests
     /// <summary>
     /// Scheduled Service Every Day At Set Time Tests.
     /// </summary>
+    [SuppressMessage("Microsoft.StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Test method names are self documenting.")]
     public class ScheduledServiceEveryDayAtSetTimeTests
     {
-        /// <summary>
-        /// Test.
-        /// </summary>
         [Fact]
-        public void Test()
+        public void CanExecute_ShouldReturnFalse_WhenTimeLessThanAtTime()
         {
             // Arrange
             var sut = new ScheduledServiceEveryDayAtSetTime(TimeSpan.Parse("02:00"));
             var now = DateTime.Parse("05/01/2019 01:00:00");
-            var last = DateTime.Parse("04/01/2019 01:00:00");
+            var last = DateTime.Parse("01/01/2019 01:00:00");
+
+            // Act
+            var result = sut.CanExecute(now, last);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void CanExecute_ShouldReturnTrue_WhenTimeGreaterThanAtTime()
+        {
+            // Arrange
+            var sut = new ScheduledServiceEveryDayAtSetTime(TimeSpan.Parse("02:00"));
+            var now = DateTime.Parse("05/01/2019 02:00:01");
+            var last = DateTime.Parse("01/01/2019 01:00:00");
+
+            // Act
+            var result = sut.CanExecute(now, last);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void CanExecute_ShouldReturnFalse_WhenTimeGreaterThanAtTimeAndLastRunToday()
+        {
+            // Arrange
+            var sut = new ScheduledServiceEveryDayAtSetTime(TimeSpan.Parse("02:00"));
+            var now = DateTime.Parse("05/01/2019 02:00:01");
+            var last = DateTime.Parse("05/01/2019 00:00:01");
+
+            // Act
+            var result = sut.CanExecute(now, last);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void CanExecute_ShouldReturnTrue_WhenTimeGreaterThanAtTimeAndLastRunYesterday()
+        {
+            // Arrange
+            var sut = new ScheduledServiceEveryDayAtSetTime(TimeSpan.Parse("02:00"));
+            var now = DateTime.Parse("05/01/2019 02:00:01");
+            var last = DateTime.Parse("04/01/2019 23:59:59");
 
             // Act
             var result = sut.CanExecute(now, last);
