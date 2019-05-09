9968923dc0fb7a9634589eb447a6064c6994bd5d
mark <sharkeymark@gmail.com>
2019-05-09 14:01:32 +0100

    fixes for tests

--- MedicalExaminer.Common/Database/AuditEntry.cs
+++ MedicalExaminer.Common/Database/AuditEntry.cs
@@ -1,16 +1,18 @@
 ﻿using MedicalExaminer.Models;
+using System;
 
 namespace MedicalExaminer.Common.Database
 {
     public class AuditEntry<T>
     {
-        public string id { get; set; }
+        public string id { get; private set; }
 
-        public T Entry { get; set; }
+        public T Entry { get; private set; }
 
         public AuditEntry(T item)
         {
             Entry = item;
+            id = Guid.NewGuid().ToString();
         }
     }
 }
