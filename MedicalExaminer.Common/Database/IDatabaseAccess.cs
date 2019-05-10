806a39e3134e1e25e982b7f944e8d339af0271bf
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-05-10 13:27:39 +0100

    Added interface implementation of update items (plural)

--- MedicalExaminer.Common/Database/IDatabaseAccess.cs
+++ MedicalExaminer.Common/Database/IDatabaseAccess.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using MedicalExaminer.Common.ConnectionSettings;
+using Microsoft.EntityFrameworkCore.Query.ExpressionVisitors.Internal;
 
 namespace MedicalExaminer.Common.Database
 {
+    /// <summary>
+    /// Database Access Interface.
+    /// </summary>
     public interface IDatabaseAccess
     {
         Task<T> CreateItemAsync<T>(
@@ -14,6 +18,15 @@ namespace MedicalExaminer.Common.Database
             bool disableAutomaticIdGeneration = false);
 
         Task<T> UpdateItemAsync<T>(IConnectionSettings connectionSettings, T item);
+
+        /// <summary>
+        /// Updates the list of items.
+        /// </summary>
+        /// <typeparam name="T">Type of item.</typeparam>
+        /// <param name="connectionSettings">The connection settings.</param>
+        /// <param name="items">The items.</param>
+        /// <returns>The list of updated items.</returns>
+        Task<IEnumerable<T>> UpdateItemsAsync<T>(IConnectionSettings connectionSettings, IEnumerable<T> items);
 
         Task<T> GetItemAsync<T>(IConnectionSettings connectionSettings, Expression<Func<T, bool>> predicate);
 
