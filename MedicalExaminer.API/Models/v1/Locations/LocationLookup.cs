36878437feada9611b316450aac95f4844247e07
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-05-15 14:15:50 +0100

    Added lookup to be used for location lookups.

---
+++ MedicalExaminer.API/Models/v1/Locations/LocationLookup.cs
@@ -0,0 +1,19 @@
+﻿namespace MedicalExaminer.API.Models.v1.Locations
+{
+    /// <summary>
+    /// Location Lookup
+    /// </summary>
+    /// <remarks>Location fields when displayed in lookup lists.</remarks>
+    public class LocationLookup
+    {
+        /// <summary>
+        /// The Location identifier.
+        /// </summary>
+        public string LocationId { get; set; }
+
+        /// <summary>
+        /// Display Name
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
