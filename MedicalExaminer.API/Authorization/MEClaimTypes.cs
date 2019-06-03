87b4720e54d4b9c59fd40165466b0ce45837b5c2
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-06-03 16:26:06 +0100

    Added new claim type

--- MedicalExaminer.API/Authorization/MEClaimTypes.cs
+++ MedicalExaminer.API/Authorization/MEClaimTypes.cs
@@ -1,7 +1,18 @@
 ﻿namespace MedicalExaminer.API.Authorization
 {
+    /// <summary>
+    /// Custom Claim Types used by MedicalExaminer.
+    /// </summary>
     public static class MEClaimTypes
     {
+        /// <summary>
+        /// The Okta User Id.
+        /// </summary>
+        public const string OktaUserId = "uid";
+
+        /// <summary>
+        /// The User Id in the Cosmos Database.
+        /// </summary>
         public const string UserId = "userId";
     }
 }
