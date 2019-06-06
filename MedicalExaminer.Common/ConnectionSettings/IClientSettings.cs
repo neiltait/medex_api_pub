64d809fbaf928a4b9a7107901d75ecdef94f9a6c
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-06-06 15:25:31 +0100

    New client settings interface that pertains just to the cosmos client.

---
+++ MedicalExaminer.Common/ConnectionSettings/IClientSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MedicalExaminer.Common.ConnectionSettings
+{
+    /// <summary>
+    /// Client Settings Interface.
+    /// </summary>
+    public interface IClientSettings
+    {
+        /// <summary>
+        /// End point URI.
+        /// </summary>
+        Uri EndPointUri { get; }
+
+        /// <summary>
+        /// Primary key.
+        /// </summary>
+        string PrimaryKey { get; }
+    }
+}
