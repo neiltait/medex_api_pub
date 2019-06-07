2a4c239454bb2e00bd6fe329458bc69156ae7154
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-06-07 12:26:15 +0100

    Added get by id to the interface.

--- MedicalExaminer.Common/Database/IDatabaseAccess.cs
+++ MedicalExaminer.Common/Database/IDatabaseAccess.cs
@@ -18,6 +18,16 @@ namespace MedicalExaminer.Common.Database
 
         Task<T> UpdateItemAsync<T>(IConnectionSettings connectionSettings, T item);
 
+        /// <summary>
+        /// Get Item By Id.
+        /// </summary>
+        /// <remarks>Faster and cheaper to call than a SQL query.</remarks>
+        /// <typeparam name="T">Type of document.</typeparam>
+        /// <param name="connectionSettings">Connection Settings.</param>
+        /// <param name="id">The ID of the document.</param>
+        /// <returns>Document.</returns>
+        Task<T> GetItemByIdAsync<T>(IConnectionSettings connectionSettings, string id);
+
         Task<T> GetItemAsync<T>(IConnectionSettings connectionSettings, Expression<Func<T, bool>> predicate);
 
         void EnsureCollectionAvailable(IConnectionSettings connectionSettings);
