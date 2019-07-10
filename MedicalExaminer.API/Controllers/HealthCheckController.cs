e58ff94d038bd6126093bf0d5fe3ed8f3d48dfa7
DanukaH <48524567+DanukaH@users.noreply.github.com>
2019-07-10 16:02:21 +0100

    Health check endpoint

---
+++ MedicalExaminer.API/Controllers/HealthCheckController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedicalExaminer.API.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("/v{api-version:apiVersion}/health-check")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> Check()
+        {
+            return Ok();
+        }
+    }
+}
