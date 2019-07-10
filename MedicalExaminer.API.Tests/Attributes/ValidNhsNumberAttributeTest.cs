adc5ec8ea4e9b55aaad564305b8d3e114d63729f
DanukaH <48524567+DanukaH@users.noreply.github.com>
2019-07-10 16:36:41 +0100

    fix failing tests

--- MedicalExaminer.API.Tests/Attributes/ValidNhsNumberAttributeTest.cs
+++ MedicalExaminer.API.Tests/Attributes/ValidNhsNumberAttributeTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using MedicalExaminer.API.Attributes;
+using MedicalExaminer.Models.Enums;
 using Moq;
 using Xunit;
 
@@ -12,7 +13,7 @@ namespace MedicalExaminer.API.Tests.Attributes
         public void Alphanumeric10DigitsNumberReturnsOk()
         {
             // Arrange
-            var nhsNumberString = "123ac 45678";
+            var nhsNumberString = "12345sa67890";
             var validationContext = new Mock<IServiceProvider>().Object;
             var sut = new ValidNhsNumberNullAllowedAttribute();
             var expectedResult = ValidationResult.Success;
@@ -28,7 +29,7 @@ namespace MedicalExaminer.API.Tests.Attributes
         public void CorrectNhsNumberReturnsNoErrors()
         {
             // Arrange
-            var nhsNumberString = "943 476 5919";
+            var nhsNumberString = "9434765919";
             var validationContext = new Mock<IServiceProvider>().Object;
             var expectedResult = ValidationResult.Success;
             var sut = new ValidNhsNumberNullAllowedAttribute();
@@ -40,14 +41,13 @@ namespace MedicalExaminer.API.Tests.Attributes
             Assert.Equal(expectedResult, result);
         }
 
-
         [Fact]
         public void ExcessDigitsInNumberReturnsErrors()
         {
             // Arrange
             var nhsNumberString = "0123456789101234";
             var validationContext = new Mock<IServiceProvider>().Object;
-            var expectedError = "Invalid NHS Number";
+            var expectedError = nameof(SystemValidationErrors.InvalidNhsNumber);
             var sut = new ValidNhsNumberNullAllowedAttribute();
 
             // Act
@@ -58,12 +58,12 @@ namespace MedicalExaminer.API.Tests.Attributes
         }
 
         [Fact]
-        public void IncorrectNhsNumberReturnsErrors()
+        public void NhsNumberWithWhiteSpacesReturnsErrors()
         {
             // Arrange
             var nhsNumberString = "987£654 4321";
             var validationContext = new Mock<IServiceProvider>().Object;
-            var expectedError = "Invalid NHS Number";
+            var expectedError = nameof(SystemValidationErrors.ContainsWhitespace);
             var sut = new ValidNhsNumberNullAllowedAttribute();
 
             // Act
@@ -80,7 +80,7 @@ namespace MedicalExaminer.API.Tests.Attributes
             var nhsNumberString = "12345";
             var validationContext = new Mock<IServiceProvider>().Object;
 
-            var expectedError = "Invalid NHS Number";
+            var expectedError = nameof(SystemValidationErrors.InvalidNhsNumber);
             var sut = new ValidNhsNumberNullAllowedAttribute();
 
             // Act
