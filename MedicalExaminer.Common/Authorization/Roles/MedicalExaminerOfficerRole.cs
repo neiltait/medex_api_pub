7a3f6cebbbc8cbb7ba10b45bea7f3ae067d105c2
DanukaH <48524567+DanukaH@users.noreply.github.com>
2019-07-10 16:43:01 +0100

    Update MedicalExaminerOfficerRole.cs

--- MedicalExaminer.Common/Authorization/Roles/MedicalExaminerOfficerRole.cs
+++ MedicalExaminer.Common/Authorization/Roles/MedicalExaminerOfficerRole.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using MedicalExaminer.Models.Enums;
+﻿using MedicalExaminer.Models.Enums;
 
 namespace MedicalExaminer.Common.Authorization.Roles
 {
