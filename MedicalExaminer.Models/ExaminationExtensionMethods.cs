daaa2f6a292ddfe53a48e0f38b75fb79b10bf695
mark <sharkeymark@gmail.com>
2019-07-23 10:37:38 +0100

    fix for the unexpected outcome

--- MedicalExaminer.Models/ExaminationExtensionMethods.cs
+++ MedicalExaminer.Models/ExaminationExtensionMethods.cs
@@ -247,10 +247,16 @@ namespace MedicalExaminer.Models
 
         public static CaseOutcomeSummary? CalculateScrutinyOutcome(this Examination examination)
         {
+            if(!examination.Unassigned && examination.CaseBreakdown.AdmissionNotes.Latest?.ImmediateCoronerReferral.Value == true)
+            {
+                return CaseOutcomeSummary.ReferToCoroner;
+            }
+
             if (!examination.CalculateCanCompleteScrutiny())
             {
                 return null;
             }
+
             if (examination.CaseBreakdown.BereavedDiscussion?.Latest?.BereavedDiscussionOutcome != null)
             {
                 if (examination.CaseBreakdown.BereavedDiscussion?.Latest?.BereavedDiscussionOutcome.Value == BereavedDiscussionOutcome.ConcernsCoronerInvestigation)
@@ -263,7 +269,6 @@ namespace MedicalExaminer.Models
                     return CaseOutcomeSummary.IssueMCCDWith100a;
                 }
             }
-        
 
             if (examination.CaseBreakdown.QapDiscussion.Latest != null)
             {
