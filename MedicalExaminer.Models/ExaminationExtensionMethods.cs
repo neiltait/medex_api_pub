9240ed61909338e5a0ef0c124be5bb2bd3fc2c4f
mark <sharkeymark@gmail.com>
2019-07-23 10:55:41 +0100

    updated the predicate

--- MedicalExaminer.Models/ExaminationExtensionMethods.cs
+++ MedicalExaminer.Models/ExaminationExtensionMethods.cs
@@ -247,7 +247,8 @@ namespace MedicalExaminer.Models
 
         public static CaseOutcomeSummary? CalculateScrutinyOutcome(this Examination examination)
         {
-            if(!examination.Unassigned && examination.CaseBreakdown.AdmissionNotes.Latest?.ImmediateCoronerReferral.Value == true)
+            if (!examination.Unassigned && examination.CaseBreakdown.AdmissionNotes.Latest?.ImmediateCoronerReferral.Value == true
+                && examination.CaseBreakdown.PreScrutiny?.Latest.OutcomeOfPreScrutiny == OverallOutcomeOfPreScrutiny.ReferToCoronerInvestigation)
             {
                 return CaseOutcomeSummary.ReferToCoroner;
             }
