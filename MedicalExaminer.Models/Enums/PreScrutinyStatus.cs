e14d18d98d41c09595e1faff876a73ce1fdd68a0
mark <sharkeymark@gmail.com>
2019-07-24 16:10:52 +0100

    added the required classes and enums

---
+++ MedicalExaminer.Models/Enums/PreScrutinyStatus.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace MedicalExaminer.Models.Enums
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+
+    public enum PreScrutinyStatus
+    {
+        PrescrutinyHappened,
+        PrescrutinyNotHappened
+    }
+}
