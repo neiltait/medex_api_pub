3d4abcf5065f771e3a67e747f08e67433bb042d9
DanukaH <48524567+DanukaH@users.noreply.github.com>
2019-07-24 14:42:54 +0100

    For Mark

--- MedicalExaminer.API/Models/v1/Report/GetCoronerReferralDownloadResponse.cs
+++ MedicalExaminer.API/Models/v1/Report/GetCoronerReferralDownloadResponse.cs
@@ -1,4 +1,6 @@
-﻿namespace MedicalExaminer.API.Models.v1.Report
+﻿using System;
+
+namespace MedicalExaminer.API.Models.v1.Report
 {
     /// <summary>
     /// the getcoronerreferraldownloadresponse
@@ -11,13 +13,56 @@
         public string GivenNames { get; set; }
 
         /// <summary>
-        /// the patients surname
+        /// The patients surname
         /// </summary>
         public string Surname { get; set; }
 
         /// <summary>
-        /// the patients nhs number
+        /// The patients nhs number
         /// </summary>
         public string NhsNumber { get; set; }
+
+        /// <summary>
+        /// Are you able to issue a MCCD? Y/N
+        /// </summary>
+        public bool AbleToIssueMCCD { get; set; }
+
+        /// <summary>
+        /// Possible cause of death established during scrutiny by Medical Examiner 1a
+        /// </summary>
+        public string CauseOfDeath1a { get; set; }
+
+        /// <summary>
+        /// Possible cause of death established during scrutiny by Medical Examiner 1b
+        /// </summary>
+        public string CauseOfDeath1b { get; set; }
+
+        /// <summary>
+        /// Possible cause of death established during scrutiny by Medical Examiner 1c
+        /// </summary>
+        public string CauseOfDeath1c { get; set; }
+
+        /// <summary>
+        /// Possible cause of death established during scrutiny by Medical Examiner 2
+        /// </summary>
+        public string CauseOfDeath2 { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Address { get; set; }
+
+        public string PlaceOfDeath { get; set; }
+
+        public DateTime DateOfDeath { get; set; }
+
+        public TimeSpan TimeOfDeath { get; set; }
+
+        public string AnyImplants { get; set; }
+
+        public string BereavedName { get; set; }
+
+        public string BereavedPhoneNumber { get; set; }
     }
 }
