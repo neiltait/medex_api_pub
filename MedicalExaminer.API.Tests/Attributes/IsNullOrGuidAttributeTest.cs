7caff8984875a2c09fb9b5a3fa2ea7aa966b8d96
DanukaH <48524567+DanukaH@users.noreply.github.com>
2019-07-30 13:35:35 +0100

    stash

---
+++ MedicalExaminer.API.Tests/Attributes/IsNullOrGuidAttributeTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Moq;
+using Xunit;
+
+namespace MedicalExaminer.API.Tests.Attributes
+{
+    public class IsNullOrGuidAttributeTest
+    {
+        [Fact]
+        public void IsNullOrGuid_When_Value_Is_Null()
+        {
+            // Arrange
+
+            var validationContext = new Mock<IServiceProvider>().Object;
+            var sut = new is();
+            var expectedResult = ValidationResult.Success;
+
+        }
+    }
+}
