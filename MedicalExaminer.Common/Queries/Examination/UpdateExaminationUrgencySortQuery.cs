fd84e875b080e33ca91e039e09d72c684c977d1f
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-08-20 14:26:56 +0100

    New service for making sure examinations are in the correct state and have an urgency sort.

---
+++ MedicalExaminer.Common/Queries/Examination/UpdateExaminationUrgencySortQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedicalExaminer.Common.Queries.Examination
+{
+    /// <summary>
+    /// Update examination urgency sort query.
+    /// </summary>
+    public class UpdateExaminationUrgencySortQuery : IQuery<bool>
+    {
+    }
+}
