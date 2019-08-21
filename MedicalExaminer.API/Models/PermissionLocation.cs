bd68cec2a57501ec9f7bb2b7731cd1802a6d6843
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-08-21 15:56:40 +0100

    Simplified permission location to contain single location.

--- MedicalExaminer.API/Models/PermissionLocation.cs
+++ MedicalExaminer.API/Models/PermissionLocation.cs
@@ -1,24 +1,16 @@
 ﻿using MedicalExaminer.Models;
-using System.Collections.Generic;
 
 namespace MedicalExaminer.API.Models
 {
     public class PermissionLocation
     {
         public readonly MEUserPermission Permission;
-        public readonly List<Location> Locations = new List<Location>();
+        public readonly Location Location;
         public readonly string UserId;
 
         public PermissionLocation(MEUserPermission permission, Location location, string userId)
         {
-            Locations.Add(location);
-            Permission = permission;
-            UserId = userId;
-        }
-
-        public PermissionLocation(MEUserPermission permission, IEnumerable<Location> locations, string userId)
-        {
-            Locations.AddRange(locations);
+            Location = location;
             Permission = permission;
             UserId = userId;
         }
