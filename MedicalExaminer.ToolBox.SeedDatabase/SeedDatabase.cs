ea1f9c94168aec634bd803e252c02ddf01e35ea2
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-09-17 12:14:09 +0100

    Seed using settings.

--- MedicalExaminer.ToolBox.SeedDatabase/SeedDatabase.cs
+++ MedicalExaminer.ToolBox.SeedDatabase/SeedDatabase.cs
@@ -1,27 +1,76 @@
-﻿using System.Threading.Tasks;
+﻿using System.IO;
+using System.Threading.Tasks;
 using MedicalExaminer.ToolBox.Common.Dtos.Generate;
 using MedicalExaminer.ToolBox.Common.Services;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace MedicalExaminer.ToolBox.SeedDatabase
 {
-    class SeedDatabase
+    /// <summary>
+    /// Seed Database
+    /// </summary>
+    internal class SeedDatabase
     {
         private readonly ILogger _logger;
 
-        private readonly GenerateConfigurationService _service;
+        private readonly ImportDocumentService _service;
+        private readonly SeedDatabaseSettings _settings;
 
-        public SeedDatabase(ILogger<SeedDatabase> logger, GenerateConfigurationService service)
+        /// <summary>
+        /// Initialise a new instance of <see cref="SeedDatabase"/>.
+        /// </summary>
+        /// <param name="logger">Logger to output to.</param>
+        /// <param name="service">Service to import documents.</param>
+        /// <param name="settings">Seed settings.</param>
+        public SeedDatabase(ILogger<SeedDatabase> logger, ImportDocumentService service, IOptions<SeedDatabaseSettings> settings)
         {
             _logger = logger;
             _service = service;
+            _settings = settings.Value;
         }
 
+        /// <summary>
+        /// Seed
+        /// </summary>
+        /// <returns></returns>
         public async Task Seed()
         {
-            _logger.LogInformation("Generating...");
+            _logger.LogInformation("Importing models...");
 
-            await _service.Generate(new GenerateConfiguration());
+            _logger.LogInformation($"Importing users from: {_settings.ImportUsersFrom}");
+
+            var countUsers = 0;
+
+            foreach (var file in Directory.EnumerateFiles(_settings.ImportUsersFrom, "*.json"))
+            {
+                var contents = File.ReadAllText(file);
+
+                var model = await _service.ImportUser(contents);
+
+                countUsers++;
+
+                _logger.LogInformation($" Imported user with id: {model.UserId}");
+            }
+
+            _logger.LogInformation($" Total:{countUsers} ");
+
+            _logger.LogInformation($"Importing users from: {_settings.ImportLocationsFrom}");
+
+            var countLocations = 0;
+
+            foreach (var file in Directory.EnumerateFiles(_settings.ImportLocationsFrom, "*.json"))
+            {
+                var contents = File.ReadAllText(file);
+
+                var model = await _service.ImportLocation(contents);
+
+                countLocations++;
+
+                _logger.LogInformation($" Imported location with id: {model.LocationId}");
+            }
+
+            _logger.LogInformation($" Total:{countLocations} ");
 
             _logger.LogInformation("Done.");
         }
