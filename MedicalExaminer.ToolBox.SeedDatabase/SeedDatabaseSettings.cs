c8b21df3214fddc89413721d400bd05cf0d61ba4
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-09-17 12:14:00 +0100

    Seed settings.

---
+++ MedicalExaminer.ToolBox.SeedDatabase/SeedDatabaseSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedicalExaminer.ToolBox.SeedDatabase
+{
+    /// <summary>
+    /// Seed Database Settings.
+    /// </summary>
+    class SeedDatabaseSettings
+    {
+        /// <summary>
+        /// Path to look for JSON files for Users.
+        /// </summary>
+        public string ImportUsersFrom { get; set; }
+
+        /// <summary>
+        /// Path to look for JSON files for Locations.
+        /// </summary>
+        public string ImportLocationsFrom { get; set; }
+    }
+}
