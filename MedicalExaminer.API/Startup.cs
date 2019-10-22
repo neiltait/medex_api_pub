72f7b86c350dcb53b800763983e6f91eae7d8dc9
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-10-22 20:45:28 +0100

    Register the service.

--- MedicalExaminer.API/Startup.cs
+++ MedicalExaminer.API/Startup.cs
@@ -441,6 +441,9 @@ example:
             services
                 .AddScoped<IAsyncQueryHandler<LocationsRetrievalByIdQuery, IEnumerable<Location>>, LocationsRetrievalByIdService
                 >();
+            services
+                .AddScoped<IAsyncQueryHandler<UpdateLocationIsMeOfficeQuery, Location>, UpdateLocationIsMeOfficeService
+                >();
         }
 
         /// <summary>
