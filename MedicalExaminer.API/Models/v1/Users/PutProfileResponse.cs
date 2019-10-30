d7e6ffcbecf40f95ae59ec328de5742bd10e7f7c
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-10-30 15:00:56 +0000

    Get and put requests and responses.

---
+++ MedicalExaminer.API/Models/v1/Users/PutProfileResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace MedicalExaminer.API.Models.v1.Users
+{
+    /// <summary>
+    /// Response for Put Profile.
+    /// </summary>
+    /// <inheritdoc />
+    public class PutProfileResponse : ResponseBase
+    {
+        /// <summary>
+        /// The User identifier.
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// The User's GMC Number.
+        /// </summary>
+        public string GmcNumber { get; set; }
+    }
+}
---
+++ MedicalExaminer.Common/Queries/User/UserUpdateProfile.cs
@@ -0,0 +1,18 @@
+﻿namespace MedicalExaminer.Common.Queries.User
+{
+    /// <summary>
+    /// User Update Profile.
+    /// </summary>
+    public class UserUpdateProfile : IUserUpdate
+    {
+        /// <summary>
+        /// User Id of user to update
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// Gmc Number.
+        /// </summary>
+        public string GmcNumber { get; set; }
+    }
+}
