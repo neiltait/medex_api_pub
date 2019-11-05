d3d068fd3a7fcf94c32859a548ff9f5bc001d984
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-11-05 11:10:56 +0000

    Fix to make the enum work as a boolean or the new enum values.

--- MedicalExaminer.Models/Enums/OpenClosedCases.cs
+++ MedicalExaminer.Models/Enums/OpenClosedCases.cs
@@ -3,6 +3,11 @@
     public enum OpenClosedCases
     {
         Open,
-        ClosedOrVoid
+        ClosedOrVoid,
+
+        // Fix to allow the original bool values to be convertible to the new enums and represent the same value.
+        // @TODO: Remove once the CMS has implement the "void case" functionality and will use the enum values intead.
+        True = Open,
+        False = ClosedOrVoid,
     }
 }
